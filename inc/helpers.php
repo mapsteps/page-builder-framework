f1493d2a1873c57d05d15d018ef11ab28093b0b6
MapSteps <connect@mapsteps.com>
2018-05-16 20:37:11 +0200

    wpbf_has_responsive_breakpoints() function

--- inc/helpers.php
+++ inc/helpers.php
@@ -181,6 +181,27 @@ function wpbf_archive_header() {
 
 }
 
+// Responsive Breakpoints
+if( !function_exists( 'wpbf_has_responsive_breakpoints' ) ) {
+
+	function wpbf_has_responsive_breakpoints() {
+
+		// stop here if premium add-on doesn't exist
+		if( !function_exists( 'wpbf_premium' ) ) return false;
+
+		// check if custom breakpoints are set, otherwise return false
+		$wpbf_settings = get_option( 'wpbf_settings' );
+
+		if ( !empty( $wpbf_settings['wpbf_breakpoint_medium'] ) || !empty( $wpbf_settings['wpbf_breakpoint_desktop'] ) ) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+}
+
 // Sidebar Right
 function wpbf_do_sidebar_right() {
 
