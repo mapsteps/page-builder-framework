26004de3fbdf4e8bda798f527b031edc5d5c41ec
MapSteps <connect@mapsteps.com>
2018-10-11 18:58:35 +0200

    minor tweaks & improvements

--- header.php
+++ header.php
@@ -8,20 +8,18 @@
 // exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-?><!doctype html>
+?><!DOCTYPE html>
 
 <html <?php language_attributes(); ?>>
 
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=0"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
+<body <?php body_class(); ?> <?php wpbf_body_schema_markup(); ?>>
 	<a class="screen-reader-text skip-link" href="#content" title="<?php echo esc_attr__( 'Skip to content', 'page-builder-framework' ); ?>"><?php _e( 'Skip to content', 'page-builder-framework' ); // WPCS: XSS ok. ?></a>
 
 	<?php do_action( 'wpbf_body_open' ); ?>
