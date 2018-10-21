197d07ea42aaccd60f5f3f31ef3934039dab3b2f
David Vongries <connect@mapsteps.com>
2018-10-21 17:05:58 +0200

    changed main navigation id

--- inc/template-parts/navigation/menu-stacked-advanced.php
+++ inc/template-parts/navigation/menu-stacked-advanced.php
@@ -34,7 +34,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 	<?php do_action( 'wpbf_before_main_menu' ); ?>
 
-	<nav id="site-navigation" class="wpbf-container wpbf-container-center wpbf-nav-wrapper" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
+	<nav navigation class="wpbf-container wpbf-container-center wpbf-nav-wrapper" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
 
 		<?php do_action( 'wpbf_main_menu_open' ); ?>
 
