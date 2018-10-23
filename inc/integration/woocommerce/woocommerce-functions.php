771d27f73f43815d64ca4e9eef5aa1830fc4758f
MapSteps <connect@mapsteps.com>
2018-10-23 17:36:22 +0200

    fixed WooCommerce grid gap setting don't take affect

--- inc/integration/woocommerce/woocommerce-functions.php
+++ inc/integration/woocommerce/woocommerce-functions.php
@@ -244,7 +244,7 @@ function wpbf_woo_product_loop_start() {
 	$mobile_breakpoint = get_theme_mod( 'woocommerce_loop_products_per_row_mobile', 1 );
 	$tablet_breakpoint = get_theme_mod( 'woocommerce_loop_products_per_row_tablet', 3 );
 	$desktop_breakpoint = get_theme_mod( 'woocommerce_loop_products_per_row_desktop', 4 );
-	$grid_gap = get_theme_mod( 'sidebar_gap', 'large' );
+	$grid_gap = get_theme_mod( 'woocommerce_loop_grid_gap', 'large' );
 
 	return '<ul class="wpbf-grid wpbf-grid-'. esc_attr( $grid_gap ) .' wpbf-grid-1-'. esc_attr( $mobile_breakpoint ) .' wpbf-grid-small-1-'. esc_attr( $tablet_breakpoint ) .' wpbf-grid-large-1-'. esc_attr( $desktop_breakpoint ) .' products">'; 
 
