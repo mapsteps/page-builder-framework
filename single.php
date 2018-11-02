92f72092ef23947f1b6db4b1fbe09ce0a18d79a1
MapSteps <connect@mapsteps.com>
2018-11-02 20:38:44 +0100

    fixed: missing loopwhen calling the_content(). Thanks @bepixeld

--- single.php
+++ single.php
@@ -116,7 +116,11 @@ get_header(); ?>
 
 			<?php else : ?>
 
+				<?php if( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
 				<?php the_content(); ?>
+
+				<?php endwhile; endif; ?>
 
 			<?php endif; ?>
 
