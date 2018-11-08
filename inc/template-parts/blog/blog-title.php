2b65ca3b3904663e23666e49b04342f625123a2c
MapSteps <connect@mapsteps.com>
2018-11-08 22:16:32 +0100

    well, sometimes it's hard to see the obvious

--- inc/template-parts/blog/blog-title.php
+++ inc/template-parts/blog/blog-title.php
@@ -13,4 +13,4 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 ?>
 
-<h3 class="entry-title" itemprop="headline"><a href="<?php esc_url( get_permalink() ) ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
+<h3 class="entry-title" itemprop="headline"><a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
