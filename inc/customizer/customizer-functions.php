8d218a0e1972352ae8984974473ebf8cf4eb6707
MapSteps <connect@mapsteps.com>
2018-11-19 14:39:11 +0100

    make customizer preview reflect the themes mobile breakpoint & custom mobile breakpoint if set

--- inc/customizer/customizer-functions.php
+++ inc/customizer/customizer-functions.php
@@ -18,17 +18,34 @@ function wpbf_adjust_customizer_responsive_sizes() {
 		$medium_breakpoint = 768;
 	}
 
+	if( function_exists( 'wpbf_breakpoint_mobile' ) ) {
+		$mobile_breakpoint = wpbf_breakpoint_mobile();
+	} else {
+		$mobile_breakpoint = 480;
+	}
+
 	$tablet_margin_left = -$medium_breakpoint/2 . 'px';
 	$tablet_width = $medium_breakpoint . 'px';
 
+	$mobile_margin_left = -$mobile_breakpoint/2 . 'px';
+	$mobile_width = $mobile_breakpoint . 'px';
+
 	?>
+
 	<style>
 		.wp-customizer .preview-tablet .wp-full-overlay-main {
 			margin-left: <?php echo esc_attr( $tablet_margin_left ); ?>;
 			width: <?php echo esc_attr( $tablet_width ); ?>;
 		}
+		.wp-customizer .preview-mobile .wp-full-overlay-main {
+			margin-left: <?php echo esc_attr( $mobile_margin_left ); ?>;
+			width: <?php echo esc_attr( $mobile_width ); ?>;
+			height: 680px;
+		}
 	</style>
+
 	<?php
+
 }
 
 add_action( 'customize_controls_print_styles', 'wpbf_adjust_customizer_responsive_sizes' );
