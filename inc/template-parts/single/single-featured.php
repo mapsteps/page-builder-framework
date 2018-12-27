e47a3329e1d2e8a20b7b7adf69b5af06325834b6
David Vongries <connect@mapsteps.com>
2018-12-27 13:50:56 +0100

    prevent featured image from being displayed if it's disabled

--- inc/template-parts/single/single-featured.php
+++ inc/template-parts/single/single-featured.php
@@ -11,6 +11,13 @@
 // exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
 
+$options = get_post_meta( get_the_ID(), 'wpbf_options', true );
+
+$remove_featured = $options ? in_array( 'remove-featured', $options ) : false;
+
+// stop here if featured image has been disabled
+if( $remove_featured ) return;
+
 the_post_thumbnail( apply_filters( 'wpbf_single_post_thumbnail_size', 'full' ), array( 'class' => 'wpbf-post-image', 'itemprop' => 'image' ) );
 
 ?>
