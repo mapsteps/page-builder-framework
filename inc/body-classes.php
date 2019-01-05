2d2aa0485746ce4687abff895494fa89237de703
David Vongries <connect@mapsteps.com>
2019-01-05 15:24:59 +0100

    refactured body class logic based on the new sidebar logic

--- inc/body-classes.php
+++ inc/body-classes.php
@@ -9,31 +9,6 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 /**
- * Global Sidebar Function
- * 
- * Helper function to define the Global Sidebar Class
- */
-function wpbf_global_sidebar_class() {
-
-	$global_sidebar_position = get_theme_mod( 'sidebar_position' );
-
-	switch ( $global_sidebar_position ) {
-		case 'left':
-			$sidebar_class = 'wpbf-sidebar-left';
-			break;
-		case 'none':
-			$sidebar_class = 'wpbf-no-sidebar';
-			break;
-		default:
-			$sidebar_class = is_page() && !is_page_template( 'page-sidebar.php' ) ? 'wpbf-no-sidebar' : 'wpbf-sidebar-right';
-			break;
-	}
-
-	return $sidebar_class;
-
-}
-
-/**
  * Body Classes
  */
 function wpbf_body_classes( $classes ) {
@@ -46,89 +21,12 @@ function wpbf_body_classes( $classes ) {
 		global $post;
 		$classes[] = 'wpbf-' . $post->post_name;
 	}
-	
-	// Sidebar Body Classes
-	$blog_sidebar_position = get_theme_mod( 'blog_sidebar_layout' );
-	$category_sidebar_position = get_theme_mod( 'category_sidebar_layout' );
-	$archive_sidebar_position = get_theme_mod( 'archive_sidebar_layout' );
-	$single_sidebar_position_global = get_theme_mod( 'single_sidebar_layout' );
 
-	if( is_singular() ) {
-
-		$single_sidebar_position = get_post_meta( $post->ID, 'wpbf_sidebar_position', true );
-
-		if( $single_sidebar_position && $single_sidebar_position !== 'global' ) {
-
-			if( $single_sidebar_position == 'right' ) {
-				$classes[] = 'wpbf-sidebar-right';
-			} elseif( $single_sidebar_position == 'left' ) {
-				$classes[] = 'wpbf-sidebar-left';
-			} elseif( $single_sidebar_position == 'none' ) {
-				$classes[] = 'wpbf-no-sidebar';
-			}
-
-		} elseif( $single_sidebar_position_global && $single_sidebar_position_global !== 'global' ) {
-
-			if( $single_sidebar_position_global == 'right' ) {
-				$classes[] = 'wpbf-sidebar-right';
-			} elseif( $single_sidebar_position_global == 'left' ) {
-				$classes[] = 'wpbf-sidebar-left';
-			} elseif( $single_sidebar_position_global == 'none' ) {
-				$classes[] = 'wpbf-no-sidebar';
-			}
-
-		} else {
-
-			$classes[] = wpbf_global_sidebar_class();
-
-		}
-
-	} elseif( is_home() ) {
-
-		if ( !$blog_sidebar_position || $blog_sidebar_position == 'global' ) {
-
-			$classes[] = wpbf_global_sidebar_class();
-
-		} elseif( $blog_sidebar_position == 'right' ) {
-			$classes[] = 'wpbf-sidebar-right';
-		} elseif( $blog_sidebar_position == 'left' ) {
-			$classes[] = 'wpbf-sidebar-left';
-		} elseif( $blog_sidebar_position == 'none' ) {
-			$classes[] = 'wpbf-no-sidebar';
-		}
-
-	} elseif( is_category() ) {
-
-		if ( !$category_sidebar_position || $category_sidebar_position == 'global' ) {
-
-			$classes[] = wpbf_global_sidebar_class();
-
-		} elseif( $category_sidebar_position == 'right' ) {
-			$classes[] = 'wpbf-sidebar-right';
-		} elseif( $category_sidebar_position == 'left' ) {
-			$classes[] = 'wpbf-sidebar-left';
-		} elseif( $category_sidebar_position == 'none' ) {
-			$classes[] = 'wpbf-no-sidebar';
-		}
-
-	} elseif ( is_archive() ) {
-
-		if ( !$archive_sidebar_position || $archive_sidebar_position == 'global' ) {
-
-			$classes[] = wpbf_global_sidebar_class();
-
-		} elseif( $archive_sidebar_position == 'right' ) {
-			$classes[] = 'wpbf-sidebar-right';
-		} elseif( $archive_sidebar_position == 'left' ) {
-			$classes[] = 'wpbf-sidebar-left';
-		} elseif( $archive_sidebar_position == 'none' ) {
-			$classes[] = 'wpbf-no-sidebar';
-		}
-
+	// Sidebar Classes
+	if( is_page() && !is_page_template( 'page-sidebar.php' ) ) {
+		$classes[] = 'wpbf-no-sidebar';
 	} else {
-
-		$classes[] = wpbf_global_sidebar_class();
-
+		$classes[] = wpbf_sidebar_layout() == 'none' ? 'wpbf-no-sidebar' : 'wpbf-sidebar-' . wpbf_sidebar_layout();
 	}
 
 	// Sub Menu Alignment Body Class
