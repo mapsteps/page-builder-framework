1120226c6e0bf262b87dfe29e1aade5161f87c71
David Vongries <connect@mapsteps.com>
2019-01-23 14:20:23 +0100

    refined Custom Menu logic

--- inc/template-parts/navigation/menu-elementor.php
+++ inc/template-parts/navigation/menu-elementor.php
@@ -9,13 +9,8 @@
 // exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-$wrapper		= get_theme_mod( 'mobile_menu_options' ) != 'menu-mobile-elementor' ? '<div class="wpbf-visible-large">' : false;
-$wrapper_close	= get_theme_mod( 'mobile_menu_options' ) != 'menu-mobile-elementor' ? '</div>' : false;
-
-echo $wrapper; // WPCS: XSS ok.
+echo '<div class="wpbf-menu-custom wpbf-visible-large">';
 
 echo do_shortcode( get_theme_mod( 'menu_custom' ) );
 
-echo $wrapper_close; // WPCS: XSS ok.
-
-?>
+echo '</div>';
--- inc/template-parts/navigation/menu-mobile-elementor.php
+++ inc/template-parts/navigation/menu-mobile-elementor.php
@@ -8,3 +8,9 @@
  
 // exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
+
+echo '<div class="wpbf-mobile-menu-custom wpbf-hidden-large">';
+
+echo do_shortcode( get_theme_mod( 'menu_custom' ) );
+
+echo '</div>';
