2939adbb1d90ed72d3770842229e8dbda8c25db7
David Vongries <connect@mapsteps.com>
2019-02-10 15:55:10 +0100

    tweaked footer logic

--- inc/template-parts/footer.php
+++ inc/template-parts/footer.php
@@ -12,15 +12,25 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 // vars
+$theme_author = apply_filters(
+	'wpbf_theme_author',
+	array(
+		'name' => __( 'Page Builder Framework', 'page-builder-framework' ),
+		'url'  => 'https://wp-pagebuilderframework.com/',
+	)
+);
+
 $footer_layout		= get_theme_mod( 'footer_layout', 'two' );
 $layout				= $footer_layout == 'one' ? ' wpbf-footer-one-column' : ' wpbf-footer-two-columns';
 $inner_layout		= $footer_layout == 'one' ? 'wpbf-inner-footer-content' : 'wpbf-inner-footer-left';
 $footer_column_one	= get_theme_mod( 'footer_column_one', '&copy; [year] - [blogname] | All rights reserved' );
-$footer_column_two	= get_theme_mod( 'footer_column_two', 'Powered by <a href="https://wp-pagebuilderframework.com/" rel="nofollow">Page Builder Framework</a>' );
-$search				= array( '[year]', '[blogname]' );
-$replace			= array( date( 'Y' ), get_option( 'blogname' ) );
+$footer_column_two	= get_theme_mod( 'footer_column_two', 'Powered by [theme_author]' );
+$search				= array( '[year]', '[blogname]', '[theme_author]' );
+$replace			= array( date( 'Y' ), get_option( 'blogname' ), '<a href="' . esc_url( $theme_author['url'] ) . '">' . $theme_author['name'] . '</a>' );
 $footer_column_one	= str_replace( $search, $replace, $footer_column_one );
 $footer_column_two	= str_replace( $search, $replace, $footer_column_two );
+$footer_column_one_layout = get_theme_mod( 'footer_column_one_layout', 'text' );
+$footer_column_two_layout = get_theme_mod( 'footer_column_two_layout', 'text' );
 
 ?>
 
@@ -34,6 +44,8 @@ $footer_column_two	= str_replace( $search, $replace, $footer_column_two );
 
 					<?php
 
+					if( $footer_column_one_layout == 'text' ) {
+
 						wp_nav_menu( array(
 							'theme_location'	=>		'footer_menu',
 							'container'			=>		false,
@@ -42,9 +54,21 @@ $footer_column_two	= str_replace( $search, $replace, $footer_column_two );
 							'fallback_cb'		=>		false,
 						));
 
-					?>
+						echo do_shortcode( apply_filters( 'footer-column-left', $footer_column_one ) );
 
-					<?php echo do_shortcode( apply_filters( 'footer-column-left', $footer_column_one ) ); ?>
+					} elseif( $footer_column_one_layout == 'menu' ) {
+
+						wp_nav_menu( array(
+							'theme_location'	=>		'footer_menu',
+							'container'			=>		false,
+							'menu_class'		=>		'wpbf-menu',
+							'depth'				=>		'1',
+							'fallback_cb'		=>		'wpbf_menu_fallback',
+						));
+
+					}
+
+					?>
 
 				</div>
 
@@ -52,9 +76,11 @@ $footer_column_two	= str_replace( $search, $replace, $footer_column_two );
 
 				<div class="wpbf-inner-footer-right">
 
-					<?php echo do_shortcode( apply_filters( 'footer-column-right', $footer_column_two ) ); ?>
-
 					<?php
+
+					if( $footer_column_two_layout == 'text' ) {
+
+						echo do_shortcode( apply_filters( 'footer-column-right', $footer_column_two ) );
 
 						wp_nav_menu( array(
 							'theme_location'	=>		'footer_menu_right',
@@ -63,6 +89,18 @@ $footer_column_two	= str_replace( $search, $replace, $footer_column_two );
 							'depth'				=>		'1',
 							'fallback_cb'		=>		false,
 						));
+
+					} elseif( $footer_column_two_layout == 'menu' ) {
+
+						wp_nav_menu( array(
+							'theme_location'	=>		'footer_menu_right',
+							'container'			=>		false,
+							'menu_class'		=>		'wpbf-menu',
+							'depth'				=>		'1',
+							'fallback_cb'		=>		'wpbf_menu_fallback',
+						));
+
+					}
 
 					?>
 
