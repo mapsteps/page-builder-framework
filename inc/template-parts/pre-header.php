fc7d3e3c5ef9c5eb53c61e7159f885c004c27e59
David Vongries <connect@mapsteps.com>
2019-02-13 00:16:09 +0100

    refactored pre-header

--- inc/template-parts/pre-header.php
+++ inc/template-parts/pre-header.php
@@ -10,13 +10,16 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 // vars
-$pre_header_layout		= get_theme_mod( 'pre_header_layout' );
-$layout					= $pre_header_layout == 'one' ? ' wpbf-pre-header-one-column' : ' wpbf-pre-header-two-columns';
-$inner_layout			= $pre_header_layout == 'one' ? 'wpbf-inner-pre-header-content' : 'wpbf-inner-pre-header-left';
-$pre_header_hook_open	= $pre_header_layout == 'one' ? 'wpbf_pre_header_open' : 'wpbf_pre_header_left_open';
-$pre_header_hook_close	= $pre_header_layout == 'one' ? 'wpbf_pre_header_close' : 'wpbf_pre_header_left_close';
-$pre_header_column_one	= get_theme_mod( 'pre_header_column_one', __( 'Column 1', 'page-builder-framework' ) );
-$pre_header_column_two	= get_theme_mod( 'pre_header_column_two', __( 'Column 2', 'page-builder-framework' ) );
+$pre_header_layout            = get_theme_mod( 'pre_header_layout' );
+$layout                       = $pre_header_layout == 'one' ? ' wpbf-pre-header-one-column' : ' wpbf-pre-header-two-columns';
+$inner_layout                 = $pre_header_layout == 'one' ? 'wpbf-inner-pre-header-content' : 'wpbf-inner-pre-header-left';
+$pre_header_hook_open         = $pre_header_layout == 'one' ? 'wpbf_pre_header_open' : 'wpbf_pre_header_left_open';
+$pre_header_hook_close        = $pre_header_layout == 'one' ? 'wpbf_pre_header_close' : 'wpbf_pre_header_left_close';
+$pre_header_column_one        = get_theme_mod( 'pre_header_column_one', __( 'Column 1', 'page-builder-framework' ) );
+$pre_header_column_two        = get_theme_mod( 'pre_header_column_two', __( 'Column 2', 'page-builder-framework' ) );
+$pre_header_column_one_layout = get_theme_mod( 'pre_header_column_one_layout', 'text' );
+$pre_header_column_two_layout = get_theme_mod( 'pre_header_column_two_layout', 'text' );
+
 
 // stop here if there's no pre-header or it has been set to none
 if( !$pre_header_layout || $pre_header_layout == 'none' ) return;
@@ -35,6 +38,8 @@ if( !$pre_header_layout || $pre_header_layout == 'none' ) return;
 
 						<?php
 
+						if( $pre_header_column_one_layout == 'text' ) {
+
 							wp_nav_menu(array(
 								'theme_location'	=>		'pre_header_menu',
 								'container'			=>		false,
@@ -43,9 +48,21 @@ if( !$pre_header_layout || $pre_header_layout == 'none' ) return;
 								'fallback_cb'		=>		false,
 							));
 
-						?>
+							echo do_shortcode( $pre_header_column_one );
 
-						<?php echo do_shortcode( $pre_header_column_one ); ?>
+						} elseif( $pre_header_column_one_layout == 'menu' ) {
+
+							wp_nav_menu(array(
+								'theme_location'	=>		'pre_header_menu',
+								'container'			=>		false,
+								'menu_class'		=>		'wpbf-menu wpbf-sub-menu wpbf-visible-large' . wpbf_sub_menu_animation(),
+								'depth'				=>		'2',
+								'fallback_cb'		=>		'wpbf_menu_fallback',
+							));
+
+						}
+
+						?>
 
 						<?php do_action( $pre_header_hook_close ); ?>
 
@@ -57,9 +74,11 @@ if( !$pre_header_layout || $pre_header_layout == 'none' ) return;
 
 						<?php do_action( 'wpbf_pre_header_right_open' ); ?>
 
-						<?php echo do_shortcode( $pre_header_column_two ); ?>
-
 						<?php
+
+						if( $pre_header_column_two_layout == 'text' ) {
+
+							echo do_shortcode( $pre_header_column_two );
 
 							wp_nav_menu(array(
 								'theme_location'	=>		'pre_header_menu_right',
@@ -68,6 +87,18 @@ if( !$pre_header_layout || $pre_header_layout == 'none' ) return;
 								'depth'				=>		'2',
 								'fallback_cb'		=>		false,
 							));
+
+						} elseif( $pre_header_column_two_layout == 'menu' ) {
+
+							wp_nav_menu(array(
+								'theme_location'	=>		'pre_header_menu_right',
+								'container'			=>		false,
+								'menu_class'		=>		'wpbf-menu wpbf-sub-menu wpbf-visible-large' . wpbf_sub_menu_animation(),
+								'depth'				=>		'2',
+								'fallback_cb'		=>		'wpbf_menu_fallback',
+							));
+
+						}
 
 						?>
 
