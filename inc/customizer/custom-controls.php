6e4ff22bccb3850f071f6a3dfe30b3151af50d0e
David Vongries <connect@mapsteps.com>
2019-02-13 00:19:15 +0100

    added input slider control & responsive input slider control

--- inc/customizer/custom-controls.php
+++ inc/customizer/custom-controls.php
@@ -12,8 +12,11 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 /**
  * Custom Controls
  */
-if( class_exists( 'WP_Customize_Control' ) ):
+if( class_exists( 'WP_Customize_Control' ) ) {
 
+	/**
+	 * Responsive Font Size Control
+	 */
 	class WPBF_Customize_Font_Size_Control extends WP_Customize_Control {
 		public $type = 'wpbf-responsive-font-size';
 
@@ -70,6 +73,9 @@ if( class_exists( 'WP_Customize_Control' ) ):
 
 	}
 
+	/**
+	 * Padding Control
+	 */
 	class WPBF_Customize_Padding_Control extends WP_Customize_Control {
 		public $type = 'wpbf-padding';
 
@@ -113,7 +119,11 @@ if( class_exists( 'WP_Customize_Control' ) ):
 
 	}
 
+	/**
+	 * Responsive Padding Control
+	 */
 	class WPBF_Customize_Responsive_Padding_Control extends WP_Customize_Control {
+
 		public $type = 'wpbf-responsive-padding';
 
 		public function enqueue() {
@@ -186,4 +196,96 @@ if( class_exists( 'WP_Customize_Control' ) ):
 
 	}
 
-endif;
+	/**
+	 * Input Slider Conltrol
+	 */
+	class WPBF_Customize_Input_Slider extends WP_Customize_Control {
+
+		public $type = 'wpbf-input-slider';
+
+		public function enqueue() {
+			wp_enqueue_script( 'wpbf-customizer-controls', WPBF_THEME_URI . '/inc/customizer/js/wpbf-customizer-controls.js', array( 'jquery' ), false, true );
+			wp_enqueue_style( 'wpbf-customizer-controls', WPBF_THEME_URI . '/inc/customizer/css/wpbf-customizer-controls.css' );
+		}
+
+		public function render_content() {
+		?>
+			<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
+			<div class="wpbf-input-slider-control">
+				<div class="slider" slider-min-value="<?php echo esc_attr( $this->choices['min'] ); ?>" slider-max-value="<?php echo esc_attr( $this->choices['max'] ); ?>" slider-step-value="<?php echo esc_attr( $this->choices['step'] ); ?>"></div><span class="slider-reset dashicons dashicons-image-rotate" slider-reset-value="<?php echo esc_attr( $this->value() ); ?>"></span>
+				<input type="text" id="<?php echo esc_attr( $this->id ); ?>" name="<?php echo esc_attr( $this->id ); ?>" value="<?php echo esc_attr( $this->value() ); ?>" class="customize-control-slider-value" <?php $this->link(); ?> />
+			</div>
+		<?php
+		}
+	}
+
+	// Register Input Slider Conltrol with Kirki
+	add_filter( 'kirki_control_types', function( $controls ) {
+		$controls['input_slider'] = 'WPBF_Customize_Input_Slider';
+		return $controls;
+	} );
+
+	/**
+	 * Responsive Input Slider Conltrol
+	 */
+	class WPBF_Customize_Responsive_Input_Slider extends WP_Customize_Control {
+
+		public $type = 'wpbf-responsive-input-slider';
+
+		public function enqueue() {
+			wp_enqueue_script( 'wpbf-customizer-controls', WPBF_THEME_URI . '/inc/customizer/js/wpbf-customizer-controls.js', array( 'jquery' ), false, true );
+			wp_enqueue_style( 'wpbf-customizer-controls', WPBF_THEME_URI . '/inc/customizer/css/wpbf-customizer-controls.css' );
+		}
+
+		public function render_content() {
+
+			$devices = array( 'desktop', 'tablet', 'mobile' );
+			$areas = array( 'top', 'right', 'bottom', 'left' );
+
+		?>
+			<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
+
+			<ul class="wpbf-responsive-options">
+				<li class="desktop">
+					<button type="button" class="preview-desktop active" data-device="desktop">
+						<i class="dashicons dashicons-desktop"></i>
+					</button>
+				</li>
+				<li class="tablet">
+					<button type="button" class="preview-tablet" data-device="tablet">
+						<i class="dashicons dashicons-tablet"></i>
+					</button>
+				</li>
+				<li class="mobile">
+					<button type="button" class="preview-mobile" data-device="mobile">
+						<i class="dashicons dashicons-smartphone"></i>
+					</button>
+				</li>
+			</ul>
+
+			<?php foreach ($devices as $device) { ?>
+
+			<div class="wpbf-control-<?php echo esc_attr( $device ); ?>">
+
+				<?php $link = $this->get_link() ?>
+
+				<?php $link = str_replace( 'mobile', $device, $link ); ?>
+
+				<?php $link = str_replace( '"', '', $link ); ?>
+
+				<div class="wpbf-input-slider-control">
+					<div class="slider" slider-min-value="<?php echo esc_attr( $this->choices['min'] ); ?>" slider-max-value="<?php echo esc_attr( $this->choices['max'] ); ?>" slider-step-value="<?php echo esc_attr( $this->choices['step'] ); ?>"></div><span class="slider-reset dashicons dashicons-image-rotate" slider-reset-value="<?php echo esc_attr( $this->value() ); ?>"></span>
+					<input type="text" id="<?php echo esc_attr( $this->id ); ?>" name="<?php echo esc_attr( $this->id ); ?>" value="<?php echo esc_attr( $this->value() ); ?>" class="customize-control-slider-value" <?php echo $link ?> />
+				</div>
+
+			</div>
+				
+			<?php
+
+			}
+
+		}
+
+	}
+
+}
--- inc/customizer/js/wpbf-customizer-controls.js
+++ inc/customizer/js/wpbf-customizer-controls.js
@@ -26,4 +26,61 @@ jQuery(document).ready(function($) {
 		$('.wpbf-control-mobile').addClass('active');
 	});
 
+	// Slider Custom Control
+	$('.wpbf-input-slider-control').each(function(){
+		var sliderValue = $(this).find('.customize-control-slider-value').val();
+		var sliderNumber = sliderValue.match(/\d+/);
+		var newSlider = $(this).find('.slider');
+		var sliderMinValue = parseFloat(newSlider.attr('slider-min-value'));
+		var sliderMaxValue = parseFloat(newSlider.attr('slider-max-value'));
+		var sliderStepValue = parseFloat(newSlider.attr('slider-step-value'));
+
+		newSlider.slider({
+			value: sliderNumber,
+			min: sliderMinValue,
+			max: sliderMaxValue,
+			step: sliderStepValue,
+			change: function(e,ui){
+				$(this).parent().find('.customize-control-slider-value').trigger('change');
+			}
+		});
+	});
+
+	// Change the value of the input field as the slider is moved
+	$('.slider').on('slide', function(event, ui) {
+		var sliderValue = $(this).parent().find('.customize-control-slider-value').val();
+		var sliderSuffix = sliderValue.replace(/\d+/g, '');
+
+		$(this).parent().find('.customize-control-slider-value').val(ui.value + sliderSuffix);
+	});
+
+	// Reset slider and input field back to the default value
+	$('.slider-reset').on('click', function() {
+		var resetValue = $(this).attr('slider-reset-value');
+		var sliderNumber = resetValue.match(/\d+/);
+		$(this).parent().find('.customize-control-slider-value').val(resetValue);
+		$(this).parent().find('.slider').slider('value', sliderNumber);
+	});
+
+	// Update slider if the input field loses focus as it's most likely changed
+	$('.customize-control-slider-value').blur(function() {
+		var resetValue = $(this).val();
+		var sliderNumber = resetValue.match(/\d+/);
+		var sliderSuffix = resetValue.replace(/\d+/g, '');
+		var slider = $(this).parent().find('.slider');
+		var sliderMinValue = parseInt(slider.attr('slider-min-value'));
+		var sliderMaxValue = parseInt(slider.attr('slider-max-value'));
+
+		// Make sure our manual input value doesn't exceed the minimum & maxmium values
+		if(sliderNumber < sliderMinValue) {
+			sliderNumber = sliderMinValue;
+			$(this).val(sliderNumber + sliderSuffix);
+		}
+		if(sliderNumber > sliderMaxValue) {
+			sliderNumber = sliderMaxValue;
+			$(this).val(sliderNumber + sliderSuffix);
+		}
+		$(this).parent().find('.slider').slider('value', sliderNumber);
+	});
+
 });
