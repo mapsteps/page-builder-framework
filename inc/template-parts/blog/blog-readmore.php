20627bb26fd492cca29da711ef06076be6497452
David Vongries <connect@mapsteps.com>
2019-02-26 13:44:58 +0100

    added screen-reader-text

--- inc/template-parts/blog/blog-readmore.php
+++ inc/template-parts/blog/blog-readmore.php
@@ -11,10 +11,15 @@
 // exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-$read_more_button = get_theme_mod( 'blog_read_more_link' ) == 'text' ? ' wpbf-inline-block' : ' wpbf-button';
-$read_more_button_primary = get_theme_mod( 'blog_read_more_link' ) == 'primary' ? ' wpbf-button-primary' : false;
-$read_more_text = get_theme_mod( 'blog_read_more_text', __( 'Read more', 'page-builder-framework' ) );
+$read_more_class = get_theme_mod( 'blog_read_more_link' ) == 'text' ? ' wpbf-inline-block' : ' wpbf-button';
+if( get_theme_mod( 'blog_read_more_link' ) == 'primary' ) $read_more_class .= ' wpbf-button-primary';
+$read_more_text = apply_filters( 'wpbf_read_more_text', get_theme_mod( 'blog_read_more_text', __( 'Read more', 'page-builder-framework' ) ) );
 
-echo '<a class="wpbf-read-more'. esc_attr( $read_more_button ) . esc_attr( $read_more_button_primary ) .'" href="'. esc_url( get_permalink( $post->ID ) ) . '">'. esc_html( $read_more_text ) .'</a>'
+echo sprintf( '<a href="%1$s" class="%2$s">%3$s%4$s</a>',
+		esc_url( get_permalink( $post->ID ) ),
+		'wpbf-read-more' . esc_attr( $read_more_class ),
+		esc_html( $read_more_text ),
+		'<span class="screen-reader-text">' . get_the_title( $post->ID ) . '</span>'
+	);
 
 ?>
