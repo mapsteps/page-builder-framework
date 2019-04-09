86ffcdcb12dd596cbed143c06ea1af525173b368
David Vongries <connect@mapsteps.com>
2019-04-09 12:30:26 +0200

    added wrapper around featured image

--- inc/template-parts/blog/blog-featured.php
+++ inc/template-parts/blog/blog-featured.php
@@ -16,6 +16,8 @@ if( !has_post_thumbnail() ) return;
 
 ?>
 
-<a class="wpbf-post-image-link" href="<?php echo esc_url( get_permalink() ); ?>">
-	<?php the_post_thumbnail( apply_filters( 'wpbf_blog_post_thumbnail_size', 'full' ), array( 'class' => 'wpbf-post-image', 'itemprop' => 'image' ) ); ?>
-</a>
+<div class="wpbf-post-image-wrapper">
+	<a class="wpbf-post-image-link" href="<?php echo esc_url( get_permalink() ); ?>">
+		<?php the_post_thumbnail( apply_filters( 'wpbf_blog_post_thumbnail_size', 'full' ), array( 'class' => 'wpbf-post-image', 'itemprop' => 'image' ) ); ?>
+	</a>
+</div>
