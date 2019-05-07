01953857603454d883092117e29635e27b455125
David Vongries <connect@mapsteps.com>
2019-05-07 09:42:17 +0200

    logic to save customizer CSS in a file instead of using inline CSS

--- inc/customizer/customizer-functions.php
+++ inc/customizer/customizer-functions.php
@@ -48,10 +48,10 @@ add_action( 'customize_controls_print_styles', 'wpbf_adjust_customizer_responsiv
  *
  * Function that's being used to minify Custom CSS
  */
-function wpbf_minify_css( $generated_css ) {
+function wpbf_minify_css( $css ) {
  
 	// Remove Comments
-	$css = preg_replace( '!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $generated_css );
+	$css = preg_replace( '!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $css );
 	// Remove space after colons
 	$css = str_replace( ': ', ':', $css );
 	$css = str_replace( ' {', '{', $css );
@@ -69,19 +69,73 @@ function wpbf_minify_css( $generated_css ) {
 function wpbf_generate_css() {
  
 	ob_start();
-	include_once( get_template_directory() . '/inc/customizer/styles.php' );
- 
+	include( get_template_directory() . '/inc/customizer/styles.php' );
 	return wpbf_minify_css( ob_get_clean() );
 
 }
+
+/**
+ * Create wpbf-customizer-styles.css file
+ */
+function wpbf_create_customizer_css_file() {
+
+	if( apply_filters( 'wpbf_css_output', 'inline' ) !== 'file' ) return;
+
+	$css = wpbf_generate_css();
+
+	require_once( ABSPATH . 'wp-admin/includes/file.php' );
+
+	global $wp_filesystem;
+
+	$upload_dir = wp_upload_dir();
+	$dir        = trailingslashit( $upload_dir['basedir'] ) . 'page-builder-framework/';
+
+	WP_Filesystem();
+
+	// create wpbf-customizer-styles.css file if it doesn't exist.
+	if( !file_exists( $upload_dir['basedir'] .'/page-builder-framework/wpbf-customizer-styles.css' ) ) {
+
+		$wp_filesystem->mkdir( $dir );
+		$wp_filesystem->put_contents( $dir . 'wpbf-customizer-styles.css', $css, 0644 );
+
+	} else {
+
+		// override the file only if changes have been made to the customizer.
+		// I'm not sure yet if it's better to override the file regardless or do this check.
+		if( $css !== $wp_filesystem->get_contents( $dir . 'wpbf-customizer-styles.css' ) ) {
+
+			$wp_filesystem->put_contents( $dir . 'wpbf-customizer-styles.css', $css, 0644 );
+
+		}
+
+	}
+
+}
+add_action( 'wp_loaded', 'wpbf_create_customizer_css_file' );
 
 /**
  * Enqueue Customizer CSS
  */
 function wpbf_customizer_frontend_scripts() {
 
-	$inline_styles = wpbf_generate_css();
-	wp_add_inline_style( apply_filters( 'wpbf_add_inline_style', 'wpbf-style' ), $inline_styles );
+	$css_output = apply_filters( 'wpbf_css_output', 'inline' );
+
+	if( $css_output === 'inline' ) {
+
+		$inline_styles = wpbf_generate_css();
+		wp_add_inline_style( apply_filters( 'wpbf_add_inline_style', 'wpbf-style' ), $inline_styles );
+
+	} elseif( $css_output === 'file' ) {
+
+		$upload_dir = wp_upload_dir();
+
+		if( file_exists( $upload_dir['basedir'] .'/page-builder-framework/wpbf-customizer-styles.css' ) ) {
+
+			wp_enqueue_style( 'wpbf-customizer', $upload_dir['baseurl'] .'/page-builder-framework/wpbf-customizer-styles.css', '', WPBF_VERSION );
+
+		}
+
+	}
 
 }
 add_action( 'wp_enqueue_scripts', 'wpbf_customizer_frontend_scripts', 11 );
@@ -89,16 +143,16 @@ add_action( 'wp_enqueue_scripts', 'wpbf_customizer_frontend_scripts', 11 );
 /**
  * Customizer CSS Live Preview
  */
-if( is_customize_preview() ) {
+function wpbf_customizer_preview_css() {
 
-	add_action( 'wp_head', 'wpbf_customizer_preview_css', 999 );
-	function wpbf_customizer_preview_css() { ?>
-		<style type='text/css'>
-		<?php require( get_template_directory() . '/inc/customizer/styles.php' ); ?>
-		</style>
-	<?php }
+	if( !is_customize_preview() ) return;
+
+	echo '<style type="text/css">';
+	require( get_template_directory() . '/inc/customizer/styles.php' );
+	echo '</style>';
 
 }
+add_action( 'wp_head', 'wpbf_customizer_preview_css', 999 );
 
 /**
  * Post Message
