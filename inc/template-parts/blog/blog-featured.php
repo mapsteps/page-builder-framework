9cf7183044f82c075caded49767c0d8accb9d3ac
David Vongries <connect@mapsteps.com>
2019-05-08 09:59:54 +0200

    added screen reader text

--- inc/template-parts/blog/blog-featured.php
+++ inc/template-parts/blog/blog-featured.php
@@ -18,6 +18,7 @@ if( !has_post_thumbnail() ) return;
 
 <div class="wpbf-post-image-wrapper">
 	<a class="wpbf-post-image-link" href="<?php echo esc_url( get_permalink() ); ?>">
+		<span class="screen-reader-text"><?php the_title() ?></span>
 		<?php the_post_thumbnail( apply_filters( 'wpbf_blog_post_thumbnail_size', 'full' ), array( 'class' => 'wpbf-post-image', 'itemprop' => 'image' ) ); ?>
 	</a>
 </div>
