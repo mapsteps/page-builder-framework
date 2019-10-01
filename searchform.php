49978e06e176a0523e1a741ed5f2eeefa9922ed8
David Vongries <connect@mapsteps.com>
2019-10-01 15:39:39 +0200

    fixed: multiple search forms on one page throws an accessibility error

--- searchform.php
+++ searchform.php
@@ -8,11 +8,19 @@
 // exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
 
+global $unique_id;
+
+if ( ! $unique_id ) {
+	$unique_id = 1;
+}
+
+$prefix  = 'searchform-';
+
 ?>
 
 <form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label>
 		<span class="screen-reader-text"><?php _e( 'Search for:', 'page-builder-framework' ); ?></span>
-		<input type="search" id="s" name="s" value="" placeholder="<?php echo esc_attr( apply_filters( 'wpbf_search_placeholder', __( 'Search &hellip;', 'page-builder-framework' ) ) ); // WPCS: XSS ok. ?>" title="<?php echo esc_attr( apply_filters( 'wpbf_search_title', __( 'Press enter to search', 'page-builder-framework' ) ) ); // WPCS: XSS ok. ?>" />
+		<input type="search" id="<?php echo esc_attr( $prefix . $unique_id++ ); ?>" name="s" value="" placeholder="<?php echo esc_attr( apply_filters( 'wpbf_search_placeholder', __( 'Search &hellip;', 'page-builder-framework' ) ) ); // WPCS: XSS ok. ?>" title="<?php echo esc_attr( apply_filters( 'wpbf_search_title', __( 'Press enter to search', 'page-builder-framework' ) ) ); // WPCS: XSS ok. ?>" />
 	</label>
 </form>
