538257099ce9a658dab883593d1965f671871153
David Vongries <connect@mapsteps.com>
2019-10-01 15:45:53 +0200

    escaping properly

--- searchform.php
+++ searchform.php
@@ -21,6 +21,6 @@ $prefix  = 'searchform-';
 <form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label>
 		<span class="screen-reader-text"><?php _e( 'Search for:', 'page-builder-framework' ); ?></span>
-		<input type="search" id="<?php echo esc_attr( $prefix . $wpbf_search_form_id++ ); ?>" name="s" value="" placeholder="<?php echo esc_attr( apply_filters( 'wpbf_search_placeholder', __( 'Search &hellip;', 'page-builder-framework' ) ) ); // WPCS: XSS ok. ?>" title="<?php echo esc_attr( apply_filters( 'wpbf_search_title', __( 'Press enter to search', 'page-builder-framework' ) ) ); // WPCS: XSS ok. ?>" />
+		<input type="search" id="<?php echo $prefix . intval( $wpbf_search_form_id++ ); ?>" name="s" value="" placeholder="<?php echo esc_attr( apply_filters( 'wpbf_search_placeholder', __( 'Search &hellip;', 'page-builder-framework' ) ) ); // WPCS: XSS ok. ?>" title="<?php echo esc_attr( apply_filters( 'wpbf_search_title', __( 'Press enter to search', 'page-builder-framework' ) ) ); // WPCS: XSS ok. ?>" />
 	</label>
 </form>
