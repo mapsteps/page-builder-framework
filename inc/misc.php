3609a05d02c4bcc9d1f2d2f2be46649cc958c37c
David Vongries <connect@mapsteps.com>
2019-10-07 14:20:55 +0200

    comments & formatting

--- inc/misc.php
+++ inc/misc.php
@@ -1,17 +1,16 @@
 <?php
 /**
- * Misc
+ * Misc.
  *
  * @package Page Builder Framework
  */
- 
-// exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) exit;
+
+defined( 'ABSPATH' ) || die( "Can't access directly" );
 
 /**
- * Main Navigation Fallback
+ * Main navigation fallback.
  *
- * is displayed if no menu is selected and user is logged in + able to edit theme options
+ * Is displayed if no menu is selected and user is logged in + able to edit theme options.
  */
 function wpbf_main_menu_fallback() {
 
@@ -21,17 +20,17 @@ function wpbf_main_menu_fallback() {
 
 		<ul class="wpbf-menu">
 		<li class="menu-item">
-		<a href="javascript:void(0)" onclick="parent.wp.customize.panel('nav_menus').focus()"><?php _e( 'Add Menu', 'page-builder-framework' ); // WPCS: XSS ok. ?></a>
+		<a href="javascript:void(0)" onclick="parent.wp.customize.panel( 'nav_menus' ).focus()"><?php _e( 'Add Menu', 'page-builder-framework' ); ?></a>
 		</li>
 		</ul>
 
 		<?php
 
-	} elseif( is_user_logged_in() && current_user_can( 'edit_theme_options' ) ) {
+	} elseif ( is_user_logged_in() && current_user_can( 'edit_theme_options' ) ) {
 
 		echo '<ul class="wpbf-menu">';
 		echo '<li class="menu-item">';
-		echo '<a href="'. esc_url( admin_url( '/nav-menus.php' ) ) .'">'. __( 'Add Menu', 'page-builder-framework' ) .'</a>'; // WPCS: XSS ok.
+		echo '<a href="' . esc_url( admin_url( '/nav-menus.php' ) ) . '">' . __( 'Add Menu', 'page-builder-framework' ) . '</a>';
 		echo '</li>';
 		echo '</ul>';
 
@@ -40,9 +39,9 @@ function wpbf_main_menu_fallback() {
 }
 
 /**
- * Mobile Navigation Fallback
+ * Mobile navigation fallback.
  *
- * is displayed if no menu is selected and user is logged in + able to edit theme options
+ * Is displayed if no menu is selected and user is logged in + able to edit theme options.
  */
 function wpbf_mobile_menu_fallback() {
 
@@ -52,17 +51,17 @@ function wpbf_mobile_menu_fallback() {
 
 		<ul class="wpbf-mobile-menu">
 		<li class="menu-item">
-		<a href="javascript:void(0)" onclick="parent.wp.customize.panel('nav_menus').focus()"><?php _e( 'Add Menu', 'page-builder-framework' ); // WPCS: XSS ok. ?></a>
+		<a href="javascript:void(0)" onclick="parent.wp.customize.panel( 'nav_menus' ).focus()"><?php _e( 'Add Menu', 'page-builder-framework' ); ?></a>
 		</li>
 		</ul>
 
 		<?php
 
-	} elseif( is_user_logged_in() && current_user_can( 'edit_theme_options' ) ) {
+	} elseif ( is_user_logged_in() && current_user_can( 'edit_theme_options' ) ) {
 
 		echo '<ul class="wpbf-menu">';
 		echo '<li class="menu-item">';
-		echo '<a href="'. esc_url( admin_url( '/nav-menus.php' ) ) .'">'. __( 'Add Menu', 'page-builder-framework' ) .'</a>'; // WPCS: XSS ok.
+		echo '<a href="' . esc_url( admin_url( '/nav-menus.php' ) ) . '">' . __( 'Add Menu', 'page-builder-framework' ) . '</a>';
 		echo '</li>';
 		echo '</ul>';
 
@@ -71,9 +70,9 @@ function wpbf_mobile_menu_fallback() {
 }
 
 /**
- * Navigation Fallback
+ * Navigation fallback.
  *
- * is displayed if no menu is selected and user is logged in + able to edit theme options
+ * Is displayed if no menu is selected and user is logged in + able to edit theme options.
  */
 function wpbf_menu_fallback() {
 
@@ -81,24 +80,31 @@ function wpbf_menu_fallback() {
 
 		?>
 
-		<a href="javascript:void(0)" onclick="parent.wp.customize.panel('nav_menus').focus()"><?php _e( 'Add Menu', 'page-builder-framework' ); // WPCS: XSS ok. ?></a>
+		<a href="javascript:void(0)" onclick="parent.wp.customize.panel( 'nav_menus' ).focus()"><?php _e( 'Add Menu', 'page-builder-framework' ); ?></a>
 
 		<?php
 
-	} elseif( is_user_logged_in() && current_user_can( 'edit_theme_options' ) ) {
+	} elseif ( is_user_logged_in() && current_user_can( 'edit_theme_options' ) ) {
 
-		echo '<a href="'. esc_url( admin_url( '/nav-menus.php' ) ) .'">'. __( 'Add Menu', 'page-builder-framework' ) .'</a>'; // WPCS: XSS ok.
+		echo '<a href="' . esc_url( admin_url( '/nav-menus.php' ) ) . '">' . __( 'Add Menu', 'page-builder-framework' ) . '</a>';
 
 	}
 
 }
 
 /**
- * Add description to main menu items
+ * Add description to main menu items.
+ *
+ * @param string $item_output The menu item's starting HTML output.
+ * @param object $item The menu item data object.
+ * @param integer $depth Depth of menu item.
+ * @param object $args The arguments.
+ *
+ * @return string The updated menu item's starting HTML output.
  */
 function wpbf_menu_description( $item_output, $item, $depth, $args ) {
 
-	if( 'main_menu' == $args->theme_location && strlen( $item->description ) > 1 ) {
+	if ( 'main_menu' === $args->theme_location && strlen( $item->description ) > 1 ) {
 
 		$item_output .= '<div class="wpbf-menu-description">' . $item->description . '</div>';
 
@@ -110,24 +116,27 @@ function wpbf_menu_description( $item_output, $item, $depth, $args ) {
 add_filter( 'walker_nav_menu_start_el', 'wpbf_menu_description', 10, 4 );
 
 /**
- * Allow HTML inside menu descriptions
+ * Allow HTML inside menu item description.
+ *
+ * @param object $menu_item The menu item object.
+ *
+ * @return object The updated menu item object.
  */
-remove_filter( 'nav_menu_description', 'strip_tags' );
+function wpbf_menu_description_html( $menu_item ) {
 
-function wpbf_menu_description_html( $menuItem ) {
+	if ( isset( $menu_item->post_type ) && 'nav_menu_item' === $menu_item->post_type ) {
 
-	if ( isset( $menuItem->post_type ) && 'nav_menu_item' == $menuItem->post_type ) {
-
-		$menuItem->description = apply_filters( 'nav_menu_description', $menuItem->post_content );
+		$menu_item->description = apply_filters( 'nav_menu_description', $menu_item->post_content );
 
 	}
-	
-	return $menuItem;
+
+	return $menu_item;
 
 }
+remove_filter( 'nav_menu_description', 'strip_tags' );
 add_filter( 'wp_setup_nav_menu_item', 'wpbf_menu_description_html' );
 
 /**
- * Remove kirki telemetry
+ * Remove Kirki telemetry.
  */
 add_filter( 'kirki_telemetry', '__return_false' );
