66a9abcb8a44a8b97f9594b4cde12b00ecc6f355
David Vongries <connect@mapsteps.com>
2019-10-07 14:27:05 +0200

    comments & formatting

--- inc/init.php
+++ inc/init.php
@@ -1,75 +1,78 @@
 <?php
 /**
- * Init
- *
- * All files are being called from here.
+ * Init.
  *
  * @package Page Builder Framework
  */
 
-// exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) exit;
+defined( 'ABSPATH' ) || die( "Can't access directly" );
 
-// Is Premium
+/**
+ * Check if Premium Add-On exists.
+ *
+ * @return bool True or false.
+ */
 function wpbf_is_premium() {
 	return function_exists( 'wpbf_premium' ) ? true : false;
 }
 
-// Backwards Compatibility
-require_once( WPBF_THEME_DIR . '/inc/backwards-compatibility.php' );
+// Backwards compatibility.
+require_once WPBF_THEME_DIR . '/inc/backwards-compatibility.php';
 
-// Options
-require_once( WPBF_THEME_DIR . '/inc/options.php' );
+// Options.
+require_once WPBF_THEME_DIR . '/inc/options.php';
 
-// Kirki Framework
-require_once( WPBF_THEME_DIR . '/assets/kirki/kirki.php' );
+// Kirki framework.
+require_once WPBF_THEME_DIR . '/assets/kirki/kirki.php';
 
-// Kirki Options
-require_once( WPBF_THEME_DIR . '/inc/customizer/wpbf-kirki.php' );
+// Kirki customizer settings.
+require_once WPBF_THEME_DIR . '/inc/customizer/wpbf-kirki.php';
 
-// Body Classes
-require_once( WPBF_THEME_DIR . '/inc/body-classes.php' );
+// Body classes.
+require_once WPBF_THEME_DIR . '/inc/body-classes.php';
 
-// Breadcrumbs
-if ( !function_exists( 'breadcrumb_trail' ) ) {
-	require_once( WPBF_THEME_DIR . '/inc/breadcrumbs.php' );
+// Breadcrumbs.
+if ( ! function_exists( 'breadcrumb_trail' ) ) {
+	require_once WPBF_THEME_DIR . '/inc/breadcrumbs.php';
 }
 
-// Helpers
-require_once( WPBF_THEME_DIR . '/inc/helpers.php' );
+// Helpers.
+require_once WPBF_THEME_DIR . '/inc/helpers.php';
 
-// Comments
-require_once( WPBF_THEME_DIR . '/inc/comments.php' );
+// Comments.
+require_once WPBF_THEME_DIR . '/inc/comments.php';
 
-// Misc
-require_once( WPBF_THEME_DIR . '/inc/misc.php' );
+// Misc.
+require_once WPBF_THEME_DIR . '/inc/misc.php';
 
-// Gutenberg
-require_once( WPBF_THEME_DIR . '/inc/integration/gutenberg/gutenberg.php' );
+// Gutenberg integration.
+require_once WPBF_THEME_DIR . '/inc/integration/gutenberg/gutenberg.php';
 
-// Customizer
-require_once( WPBF_THEME_DIR . '/inc/customizer/customizer-functions.php' );
+// Customizer functions.
+require_once WPBF_THEME_DIR . '/inc/customizer/customizer-functions.php';
 
-// Theme Mods
-require_once( WPBF_THEME_DIR . '/inc/theme-mods.php' );
+// Theme mods.
+require_once WPBF_THEME_DIR . '/inc/theme-mods.php';
 
-// Header Footer Elementor
-if( !function_exists( 'wpbf_header_footer_elementor_support' ) ) { // backwards compatibility check as this was earlier included in Premium. Will be removed at some point.
-	require_once( WPBF_THEME_DIR . '/inc/integration/header-footer-elementor.php' );
+// Header/Footer Elementor integration.
+if ( ! function_exists( 'wpbf_header_footer_elementor_support' ) ) {
+	// backwards compatibility check as this was earlier included in Premium.
+	// Will be removed at some point.
+	require_once WPBF_THEME_DIR . '/inc/integration/header-footer-elementor.php';
 }
 
-// Easy Digital Downloads
-if( class_exists( 'Easy_Digital_Downloads' ) ) {
-	require_once( WPBF_THEME_DIR . '/inc/integration/edd/edd.php' );
+// Easy Digital Downloads.
+if ( class_exists( 'Easy_Digital_Downloads' ) ) {
+	require_once WPBF_THEME_DIR . '/inc/integration/edd/edd.php';
 }
 
-// WooCommerce
-if( class_exists( 'WooCommerce' ) ) {
-	require_once( WPBF_THEME_DIR . '/inc/integration/woocommerce/woocommerce.php' );
+// WooCommerce.
+if ( class_exists( 'WooCommerce' ) ) {
+	require_once WPBF_THEME_DIR . '/inc/integration/woocommerce/woocommerce.php';
 }
 
 /**
- * Pre Header
+ * Render pre header.
  */
 function wpbf_do_pre_header() {
 	get_template_part( 'inc/template-parts/pre-header' );
@@ -77,7 +80,7 @@ function wpbf_do_pre_header() {
 add_action( 'wpbf_pre_header', 'wpbf_do_pre_header' );
 
 /**
- * Header
+ * Render header.
  */
 function wpbf_do_header() {
 	get_template_part( 'inc/template-parts/header' );
@@ -85,7 +88,7 @@ function wpbf_do_header() {
 add_action( 'wpbf_header', 'wpbf_do_header' );
 
 /**
- * Footer
+ * Render footer.
  */
 function wpbf_do_footer() {
 	get_template_part( 'inc/template-parts/footer' );
@@ -93,7 +96,7 @@ function wpbf_do_footer() {
 add_action( 'wpbf_footer', 'wpbf_do_footer' );
 
 /**
- * 404 Page
+ * Render 404 page.
  */
 function wpbf_do_404() {
 	get_template_part( 'inc/template-parts/404' );
