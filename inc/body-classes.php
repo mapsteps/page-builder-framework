b6a767ad3427175dbfc1ad93b9adedc373a0abc2
David Vongries <connect@mapsteps.com>
2019-10-07 15:10:29 +0200

    comments & formatting

--- inc/body-classes.php
+++ inc/body-classes.php
@@ -1,42 +1,46 @@
 <?php
 /**
- * Body Classes
+ * Body classes.
  *
  * @package Page Builder Framework
  */
- 
-// exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) exit;
+
+defined( 'ABSPATH' ) || die( "Can't access directly" );
 
 /**
- * Body Classes
+ * Body classes.
+ *
+ * @param array $classes The body classes.
+ *
+ * @return array The updated body classes.
  */
 function wpbf_body_classes( $classes ) {
 
-	// Add wpbf Body Class
+	// Add wpbf body class.
 	$classes[] = 'wpbf';
 
-	// Add wpbf-{post-name} Body Class on Singular
-	if( is_singular() ) {
+	// Add wpbf-{post-name} body class on singular.
+	if ( is_singular() ) {
 		global $post;
 		$classes[] = 'wpbf-' . $post->post_name;
 	}
 
-	// Sidebar Classes
-	if( is_page() && !is_page_template( 'page-sidebar.php' ) ) {
+	// Sidebar classes.
+	if ( is_page() && ! is_page_template( 'page-sidebar.php' ) ) {
 		$classes[] = 'wpbf-no-sidebar';
 	} else {
 		$classes[] = wpbf_sidebar_layout() == 'none' ? 'wpbf-no-sidebar' : 'wpbf-sidebar-' . wpbf_sidebar_layout();
 	}
 
-	// Full Width Body Class
+	// Full width body class.
 	$inner_content = wpbf_inner_content( $echo = false );
 
-	if( !$inner_content ) {
+	if ( ! $inner_content ) {
 		$classes[] = 'wpbf-full-width';
 	}
 
-	if( get_theme_mod( 'woocommerce_loop_layout' ) == 'list' ) {
+	// WooCommerce list layout.
+	if ( 'list' === get_theme_mod( 'woocommerce_loop_layout' ) ) {
 		$classes[] = 'wpbf-woo-list-view';
 	}
 
@@ -46,11 +50,15 @@ function wpbf_body_classes( $classes ) {
 add_filter( 'body_class', 'wpbf_body_classes' );
 
 /**
- * Post Classes
+ * Post classes.
+ *
+ * @param array $classes The post classes.
+ *
+ * @return array The updated post classes.
  */
 function wpbf_post_classes( $classes ) {
 
-	// Add wpbf-post class to all Posts
+	// Add wpbf-post class to all posts.
 	$classes[] = 'wpbf-post';
 
 	return $classes;
