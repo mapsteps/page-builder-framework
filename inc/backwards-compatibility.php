490fe05c3052634beafdf709799716a6c87f288b
David Vongries <connect@mapsteps.com>
2019-10-07 15:13:32 +0200

    comments & formatting

--- inc/backwards-compatibility.php
+++ inc/backwards-compatibility.php
@@ -1,12 +1,11 @@
 <?php
 /**
- * Backwards Compatibility
+ * Backwards compatibility.
  *
  * @package Page Builder Framework
  */
 
-// exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) exit;
+defined( 'ABSPATH' ) || die( "Can't access directly" );
 
 $menu_logo_font_size             = get_theme_mod( 'menu_logo_font_size' );
 $menu_logo_description_font_size = get_theme_mod( 'menu_logo_description_font_size' );
@@ -15,32 +14,32 @@ $sidebar_widget_padding_right    = get_theme_mod( 'sidebar_widget_padding_right'
 $sidebar_widget_padding_bottom   = get_theme_mod( 'sidebar_widget_padding_bottom' );
 $sidebar_widget_padding_left     = get_theme_mod( 'sidebar_widget_padding_left' );
 
-if( $menu_logo_font_size ) {
+if ( $menu_logo_font_size ) {
 	set_theme_mod( 'menu_logo_font_size_desktop', $menu_logo_font_size );
 	remove_theme_mod( 'menu_logo_font_size' );
 }
 
-if( $menu_logo_description_font_size ) {
+if ( $menu_logo_description_font_size ) {
 	set_theme_mod( 'menu_logo_description_font_size_desktop', $menu_logo_description_font_size );
 	remove_theme_mod( 'menu_logo_description_font_size' );
 }
 
-if( $sidebar_widget_padding_top ) {
+if ( $sidebar_widget_padding_top ) {
 	set_theme_mod( 'sidebar_widget_padding_top_desktop', $sidebar_widget_padding_top );
 	remove_theme_mod( 'sidebar_widget_padding_top' );
 }
 
-if( $sidebar_widget_padding_right ) {
+if ( $sidebar_widget_padding_right ) {
 	set_theme_mod( 'sidebar_widget_padding_right_desktop', $sidebar_widget_padding_right );
 	remove_theme_mod( 'sidebar_widget_padding_right' );
 }
 
-if( $sidebar_widget_padding_bottom ) {
+if ( $sidebar_widget_padding_bottom ) {
 	set_theme_mod( 'sidebar_widget_padding_bottom_desktop', $sidebar_widget_padding_bottom );
 	remove_theme_mod( 'sidebar_widget_padding_bottom' );
 }
 
-if( $sidebar_widget_padding_left ) {
+if ( $sidebar_widget_padding_left ) {
 	set_theme_mod( 'sidebar_widget_padding_left_desktop', $sidebar_widget_padding_left );
 	remove_theme_mod( 'sidebar_widget_padding_left' );
 }
@@ -50,7 +49,7 @@ $blog_author   = get_theme_mod( 'blog_author' );
 $single_author = get_theme_mod( 'single_author' );
 $blog_comments = get_theme_mod( 'blog_comments' );
 
-if( $blog_author === 'hide' || $single_author === 'hide' ) {
+if ( 'hide' === $blog_author || 'hide' === $single_author ) {
 
 	$article_meta = array_diff( $article_meta, array( 'author' ) );
 	$article_meta = array_values( $article_meta );
@@ -61,7 +60,7 @@ if( $blog_author === 'hide' || $single_author === 'hide' ) {
 
 }
 
-if( $blog_comments === 'show' ) {
+if ( 'show' === $blog_comments ) {
 
 	$article_meta[] = 'comments';
 
