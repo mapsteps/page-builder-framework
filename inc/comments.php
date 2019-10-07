6702eb77b264497ba6fb95461184eb7ea58a0b38
David Vongries <connect@mapsteps.com>
2019-10-07 15:05:25 +0200

    comments & formatting, fixed: duplicate closing <li> tag

--- inc/comments.php
+++ inc/comments.php
@@ -1,91 +1,99 @@
 <?php
 /**
- * Comments
+ * Comments.
  *
  * @package Page Builder Framework
  */
- 
-// exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) exit;
 
+defined( 'ABSPATH' ) || die( "Can't access directly" );
+
+/**
+ * Comments callback.
+ *
+ * @param Object $comment WP_Comment.
+ * @param array $args The arguments.
+ * @param integer $depth The depth.
+ */
 function wpbf_comments( $comment, $args, $depth ) {
 
-	if ( 'pingback' == $comment->comment_type || 'trackback' == $comment->comment_type ) { ?>
+	// Pingbacks & Trackbacks.
+	if ( 'pingback' === $comment->comment_type || 'trackback' === $comment->comment_type ) { ?>
 
-	<li id="comment-<?php comment_ID(); ?>">
+		<li id="comment-<?php comment_ID(); ?>">
 
-		<article <?php comment_class(); ?>>
+			<article <?php comment_class(); ?>>
 
-			<?php _e( 'Pingback:', 'page-builder-framework' ); // WPCS: XSS ok. ?>
+				<?php _e( 'Pingback:', 'page-builder-framework' ); ?>
 
-			<?php comment_author_link(); ?>
+				<?php comment_author_link(); ?>
 
-			<?php edit_comment_link(); ?>
+				<?php edit_comment_link(); ?>
 
-		</article>
+			</article>
 
-	</li>
+		</li>
 
 	<?php } else { ?>
 
-	<li id="comment-<?php comment_ID(); ?>">
+		<li id="comment-<?php comment_ID(); ?>">
 
-		<article <?php comment_class(); ?> itemscope="itemscope" itemtype="https://schema.org/Comment">
+			<article <?php comment_class(); ?> itemscope="itemscope" itemtype="https://schema.org/Comment">
 
-			<footer class="comment-meta">
+				<footer class="comment-meta">
 
-				<?php echo get_avatar( $comment, 120 ); ?>
+					<?php echo get_avatar( $comment, 120 ); ?>
 
-				<div class="comment-author-info">
+					<div class="comment-author-info">
 
-					<span class="comment-author author vcard" itemprop="author" itemscope itemtype="https://schema.org/Person">
+						<span class="comment-author author vcard" itemprop="author" itemscope itemtype="https://schema.org/Person">
 
-					<?php if ( $comment->user_id ) {
+						<?php
 
-						$user = get_userdata( $comment->user_id ); ?>
-						<?php printf( '<cite itemprop="name" class="fn">%s</cite>', esc_html( $user->display_name ) ); ?>
+						if ( $comment->user_id ) {
+							$user = get_userdata( $comment->user_id );
+							printf( '<cite itemprop="name" class="fn">%s</cite>', esc_html( $user->display_name ) );
+						} else {
+							printf( '<cite itemprop="name" class="fn">%s</cite>', get_comment_author_link() );
+						}
 
-					<?php } else { ?>
+						?>
 
-						<?php printf( '<cite itemprop="name" class="fn">%s</cite>', get_comment_author_link() ); ?>
+						</span>
 
-					<?php } ?>
+						<?php edit_comment_link( __( 'Edit', 'page-builder-framework' ) ); ?>
 
-					</span>
+						<time class="comment-time" datetime="<?php comment_time( 'c' ); ?>" itemprop="datePublished">
 
-					<?php edit_comment_link( __( 'Edit', 'page-builder-framework' ) ); ?>
+							<a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>"><?php comment_time( __( 'F d, Y', 'page-builder-framework' ) ); ?></a>
 
-					<time class="comment-time" datetime="<?php comment_time( 'c' ); ?>" itemprop="datePublished">
+						</time>
 
-						<a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>"><?php comment_time( __( 'F d, Y', 'page-builder-framework' ) ); ?></a>
+					</div>
 
-					</time>
+				</footer>
+
+				<?php if ( $comment->comment_approved == '0' ) : ?>
+
+				<div class="wpbf-notice wpbf-notice-warning">
+
+					<p><?php _e( 'Your comment has yet to be approved.', 'page-builder-framework' ); ?></p>
 
 				</div>
 
-			</footer>
+				<?php endif; ?>
 
-			<?php if ( $comment->comment_approved == '0' ) : ?>
+				<div class="comment-content" itemprop="text">
 
-			<div class="wpbf-notice wpbf-notice-warning">
+					<?php comment_text() ?>
 
-				<p><?php _e( 'Your comment has yet to be approved.', 'page-builder-framework' ); // WPCS: XSS ok. ?></p>
+				</div>
 
-			</div>
+				<?php comment_reply_link( array_merge( $args, array( 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
 
-			<?php endif; ?>
+			</article>
 
-			<div class="comment-content" itemprop="text">
+		<?php // We're not closing the li tag right here. WordPress does it for us.
 
-				<?php comment_text() ?>
-
-			</div>
-
-			<?php comment_reply_link( array_merge( $args, array( 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ) ?>
-
-		</article>
-	</li>
-
-	<?php }
+	}
 
 }
