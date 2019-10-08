1b1018cd16a00ab2ca84f4a06423503871d48b37
David Vongries <connect@mapsteps.com>
2019-10-08 11:41:42 +0200

    comments & formatting

--- inc/integration/header-footer-elementor.php
+++ inc/integration/header-footer-elementor.php
@@ -1,68 +1,59 @@
 <?php
 /**
- * Header Footer Elementor
+ * Header Footer Elementor integration.
+ *
+ * https://wordpress.org/plugins/header-footer-elementor/
  *
  * @package Page Builder Framework Premium Add-On
  * @subpackage Integration
  */
- 
-// exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) exit;
+
+defined( 'ABSPATH' ) || die( "Can't access directly" );
 
 /**
- * Render Header if HFE Header Template Exists
+ * Render header if HFE header termplate exists.
  */
 function wpbf_do_hfe_header() {
 
 	if ( function_exists( 'hfe_render_header' ) && hfe_header_enabled() ) {
-
 		hfe_render_header();
-
 	}
 
 }
 add_action( 'wpbf_header', 'wpbf_do_hfe_header' );
 
 /**
- * Render Footer if HFE Footer Template Exists
+ * Render footer if HFE footer template exists.
  */
 function wpbf_do_hfe_footer() {
-	
+
 	if ( function_exists( 'hfe_render_footer' ) && hfe_footer_enabled() ) {
-
 		hfe_render_footer();
-
 	}
 
 }
 add_action( 'wpbf_footer', 'wpbf_do_hfe_footer' );
 
 /**
- * Remove Theme Header/Footer if the respective HFE Template is present
+ * Remove theme header/footer if the respective HFE template is present.
  */
 function wpbf_remove_header_footer_hfe() {
 
 	if ( function_exists( 'hfe_render_header' ) && hfe_header_enabled() ) {
-
 		remove_action( 'wpbf_header', 'wpbf_do_header' );
-
 	}
 
 	if ( function_exists( 'hfe_render_footer' ) && hfe_footer_enabled() ) {
-
 		remove_action( 'wpbf_footer', 'wpbf_do_footer' );
-
 	}
 
 }
 add_action( 'wp', 'wpbf_remove_header_footer_hfe' );
 
 /**
- * Add HFE Theme Support
+ * Add HFE theme support.
  */
 function wpbf_header_footer_elementor() {
-
 	add_theme_support( 'header-footer-elementor' );
-
 }
 add_action( 'after_setup_theme', 'wpbf_header_footer_elementor' );
