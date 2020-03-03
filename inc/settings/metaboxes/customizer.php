c0646c5f64ed6b9897717af7468be471a98a051c
Bagus <25834188+contactjavas@users.noreply.github.com>
2020-03-04 00:11:38 +0700

    make customizer links filterable and actionable (the filter name couldn't be the same as action name)

--- inc/settings/metaboxes/customizer.php
+++ inc/settings/metaboxes/customizer.php
@@ -6,6 +6,95 @@
  */
 
 defined( 'ABSPATH' ) || die( "Can't access directly" );
+
+/**
+ * Output customizer links.
+ */
+function wpbf_customizer_links() {
+
+	$customizer_links = array(
+		array(
+			array(
+				'text' => __( 'Logo', 'page-builder-framework' ),
+				'url'  => admin_url( 'customize.php?autofocus%5Bsection%5D=title_tagline' ),
+			),
+			array(
+				'text' => __( 'Site Navigation', 'page-builder-framework' ),
+				'url'  => admin_url( 'customize.php?autofocus%5Bsection%5D=wpbf_menu_options' ),
+			),
+		),
+		array(
+			array(
+				'text' => __( 'Header', 'page-builder-framework' ),
+				'url'  => admin_url( 'customize.php?autofocus%5Bpanel%5D=header_panel' ),
+			),
+			array(
+				'text' => __( 'Footer', 'page-builder-framework' ),
+				'url'  => admin_url( 'customize.php?autofocus%5Bsection%5D=wpbf_footer_options' ),
+			),
+		),
+		array(
+			array(
+				'text' => __( 'Layout', 'page-builder-framework' ),
+				'url'  => admin_url( 'customize.php?autofocus%5Bsection%5D=wpbf_page_options' ),
+			),
+			array(
+				'text' => __( 'Sidebar', 'page-builder-framework' ),
+				'url'  => admin_url( 'customize.php?autofocus%5Bsection%5D=wpbf_sidebar_options' ),
+			),
+		),
+		array(
+			array(
+				'text' => __( 'Blog', 'page-builder-framework' ),
+				'url'  => admin_url( 'customize.php?autofocus%5Bpanel%5D=blog_panel' ),
+			),
+			array(
+				'text' => __( 'Post Layout', 'page-builder-framework' ),
+				'url'  => admin_url( 'customize.php?autofocus%5Bsection%5D=wpbf_single_options' ),
+			),
+		),
+		array(
+			array(
+				'text' => __( 'Typography', 'page-builder-framework' ),
+				'url'  => admin_url( 'customize.php?autofocus%5Bpanel%5D=typo_panel' ),
+			),
+			array(
+				'text' => __( 'Theme Buttons', 'page-builder-framework' ),
+				'url'  => admin_url( 'customize.php?autofocus%5Bsection%5D=wpbf_button_options' ),
+			),
+		),
+	);
+
+	$customizer_links = apply_filters( 'wpbf_customizer_link_items', $customizer_links );
+
+	foreach ( $customizer_links as $link_columns ) {
+		?>
+
+		<li class="list-item">
+			<div class="customizer-items">
+
+				<?php
+				foreach ( $link_columns as $link_item ) {
+					?>
+
+					<div class="customizer-item">
+						<a href="<?php echo esc_url( $link_item['url'] ); ?>">
+							<?php echo esc_html( $link_item['text'] ); ?>
+						</a>
+					</div>
+
+					<?php
+				}
+				?>
+
+			</div>
+		</li>
+
+		<?php
+	}
+
+}
+add_action( 'wpbf_customizer_links', 'wpbf_customizer_links' );
 ?>
 
 <div class="neatbox is-smooth has-medium-gap has-bigger-heading customizer-box">
@@ -13,86 +102,12 @@ defined( 'ABSPATH' ) || die( "Can't access directly" );
 		Links to Customizer Settings
 	</h2>
 	<ul class="neatbox-list customizer-list">
-		<li class="list-item">
 
-			<div class="customizer-items">
-				<div class="customizer-item">
-					<a href="<?php echo esc_url( admin_url() ); ?>/customize.php?autofocus%5Bsection%5D=title_tagline">
-						<?php _e( 'Logo', 'page-builder-framework' ); ?>
-					</a>
-				</div>
-				<div class="customizer-item">
-					<a href="<?php echo esc_url( admin_url() ); ?>/customize.php?autofocus%5Bsection%5D=wpbf_menu_options">
-						<?php _e( 'Site Navigation', 'page-builder-framework' ); ?>
-					</a>
-				</div>
-			</div>
-
-		</li>
-		<li class="list-item">
-
-			<div class="customizer-items">
-				<div class="customizer-item">
-					<a href="<?php echo esc_url( admin_url( 'customize.php?autofocus%5Bpanel%5D=header_panel' ) ); ?>">
-						<?php _e( 'Header', 'page-builder-framework' ); ?>
-					</a>
-				</div>
-				<div class="customizer-item">
-					<a href="<?php echo esc_url( admin_url( 'customize.php?autofocus%5Bsection%5D=wpbf_footer_options' ) ); ?>">
-						<?php _e( 'Footer', 'page-builder-framework' ); ?>
-					</a>
-				</div>
-			</div>
-
-		</li>
-		<li class="list-item">
-
-			<div class="customizer-items">
-				<div class="customizer-item">
-					<a href="<?php echo esc_url( admin_url( 'customize.php?autofocus%5Bsection%5D=wpbf_page_options' ) ); ?>">
-						<?php _e( 'Layout', 'page-builder-framework' ); ?>
-					</a>
-				</div>
-				<div class="customizer-item">
-					<a href="<?php echo esc_url( admin_url( 'customize.php?autofocus%5Bsection%5D=wpbf_sidebar_options' ) ); ?>">
-						<?php _e( 'Sidebar', 'page-builder-framework' ); ?>
-					</a>
-				</div>
-			</div>
-
-		</li>
-		<li class="list-item">
-
-			<div class="customizer-items">
-				<div class="customizer-item">
-					<a href="<?php echo esc_url( admin_url( 'customize.php?autofocus%5Bpanel%5D=blog_panel' ) ); ?>">
-						<?php _e( 'Blog', 'page-builder-framework' ); ?>
-					</a>
-				</div>
-				<div class="customizer-item">
-					<a href="<?php echo esc_url( admin_url( 'customize.php?autofocus%5Bsection%5D=wpbf_single_options' ) ); ?>">
-						<?php _e( 'Post Layout', 'page-builder-framework' ); ?>
-					</a>
-				</div>
-			</div>
-
-		</li>
-		<li class="list-item">
-
-			<div class="customizer-items">
-				<div class="customizer-item">
-					<a href="<?php echo esc_url( admin_url( 'customize.php?autofocus%5Bpanel%5D=typo_panel' ) ); ?>">
-						<?php _e( 'Typography', 'page-builder-framework' ); ?>
-					</a>
-				</div>
-				<div class="customizer-item">
-					<a href="<?php echo esc_url( admin_url( 'customize.php?autofocus%5Bsection%5D=wpbf_button_options' ) ); ?>">
-						<?php _e( 'Theme Buttons', 'page-builder-framework' ); ?>
-					</a>
-				</div>
-			</div>
-
-		</li>
+		<?php
+		do_action( 'wpbf_before_customizer_links' );
+		do_action( 'wpbf_customizer_links' );
+		do_action( 'wpbf_after_customizer_links' );
+		?>
 
 		<li class="list-item inline-action">
 			<div class="customizer-item list-content">
