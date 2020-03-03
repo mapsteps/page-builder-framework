dd9b78845ce4fba2995c66ed8db85af95732b843
Bagus <25834188+contactjavas@users.noreply.github.com>
2020-03-04 00:46:44 +0700

    flatten the array and simplify the markups

--- inc/settings/metaboxes/customizer.php
+++ inc/settings/metaboxes/customizer.php
@@ -14,77 +14,55 @@ function wpbf_do_customizer_links() {
 
 	$customizer_links = array(
 		array(
-			array(
-				'text' => __( 'Logo', 'page-builder-framework' ),
-				'url'  => admin_url( 'customize.php?autofocus%5Bsection%5D=title_tagline' ),
-			),
-			array(
-				'text' => __( 'Site Navigation', 'page-builder-framework' ),
-				'url'  => admin_url( 'customize.php?autofocus%5Bsection%5D=wpbf_menu_options' ),
-			),
+			'text' => __( 'Logo', 'page-builder-framework' ),
+			'url'  => admin_url( 'customize.php?autofocus%5Bsection%5D=title_tagline' ),
 		),
 		array(
-			array(
-				'text' => __( 'Header', 'page-builder-framework' ),
-				'url'  => admin_url( 'customize.php?autofocus%5Bpanel%5D=header_panel' ),
-			),
-			array(
-				'text' => __( 'Footer', 'page-builder-framework' ),
-				'url'  => admin_url( 'customize.php?autofocus%5Bsection%5D=wpbf_footer_options' ),
-			),
+			'text' => __( 'Site Navigation', 'page-builder-framework' ),
+			'url'  => admin_url( 'customize.php?autofocus%5Bsection%5D=wpbf_menu_options' ),
 		),
 		array(
-			array(
-				'text' => __( 'Layout', 'page-builder-framework' ),
-				'url'  => admin_url( 'customize.php?autofocus%5Bsection%5D=wpbf_page_options' ),
-			),
-			array(
-				'text' => __( 'Sidebar', 'page-builder-framework' ),
-				'url'  => admin_url( 'customize.php?autofocus%5Bsection%5D=wpbf_sidebar_options' ),
-			),
+			'text' => __( 'Header', 'page-builder-framework' ),
+			'url'  => admin_url( 'customize.php?autofocus%5Bpanel%5D=header_panel' ),
 		),
 		array(
-			array(
-				'text' => __( 'Blog', 'page-builder-framework' ),
-				'url'  => admin_url( 'customize.php?autofocus%5Bpanel%5D=blog_panel' ),
-			),
-			array(
-				'text' => __( 'Post Layout', 'page-builder-framework' ),
-				'url'  => admin_url( 'customize.php?autofocus%5Bsection%5D=wpbf_single_options' ),
-			),
+			'text' => __( 'Footer', 'page-builder-framework' ),
+			'url'  => admin_url( 'customize.php?autofocus%5Bsection%5D=wpbf_footer_options' ),
 		),
 		array(
-			array(
-				'text' => __( 'Typography', 'page-builder-framework' ),
-				'url'  => admin_url( 'customize.php?autofocus%5Bpanel%5D=typo_panel' ),
-			),
-			array(
-				'text' => __( 'Theme Buttons', 'page-builder-framework' ),
-				'url'  => admin_url( 'customize.php?autofocus%5Bsection%5D=wpbf_button_options' ),
-			),
+			'text' => __( 'Layout', 'page-builder-framework' ),
+			'url'  => admin_url( 'customize.php?autofocus%5Bsection%5D=wpbf_page_options' ),
+		),
+		array(
+			'text' => __( 'Sidebar', 'page-builder-framework' ),
+			'url'  => admin_url( 'customize.php?autofocus%5Bsection%5D=wpbf_sidebar_options' ),
+		),
+		array(
+			'text' => __( 'Blog', 'page-builder-framework' ),
+			'url'  => admin_url( 'customize.php?autofocus%5Bpanel%5D=blog_panel' ),
+		),
+		array(
+			'text' => __( 'Post Layout', 'page-builder-framework' ),
+			'url'  => admin_url( 'customize.php?autofocus%5Bsection%5D=wpbf_single_options' ),
+		),
+		array(
+			'text' => __( 'Typography', 'page-builder-framework' ),
+			'url'  => admin_url( 'customize.php?autofocus%5Bpanel%5D=typo_panel' ),
+		),
+		array(
+			'text' => __( 'Theme Buttons', 'page-builder-framework' ),
+			'url'  => admin_url( 'customize.php?autofocus%5Bsection%5D=wpbf_button_options' ),
 		),
 	);
 
-	foreach ( $customizer_links as $link_columns ) {
+	foreach ( $customizer_links as $link_item ) {
 		?>
 
 		<li class="list-item">
-			<div class="customizer-items">
-
-				<?php
-				foreach ( $link_columns as $link_item ) {
-					?>
-
-					<div class="customizer-item">
-						<a href="<?php echo esc_url( $link_item['url'] ); ?>">
-							<?php echo esc_html( $link_item['text'] ); ?>
-						</a>
-					</div>
-
-					<?php
-				}
-				?>
-
+			<div class="list-content">
+				<a href="<?php echo esc_url( $link_item['url'] ); ?>">
+					<?php echo esc_html( $link_item['text'] ); ?>
+				</a>
 			</div>
 		</li>
 
@@ -108,7 +86,7 @@ add_action( 'wpbf_customizer_links', 'wpbf_do_customizer_links' );
 		?>
 
 		<li class="list-item inline-action">
-			<div class="customizer-item list-content">
+			<div class="list-content">
 				<h3 class="title">Launch WordPress Customizer</h3>
 				<p class="description">
 					Explore all of the Page Builder Framework features.
