28f6a69e0701a397a0429f32b8cee3158f62bf80
Mackenzie Morgan <macoafi@gmail.com>
2020-04-14 22:08:31 -0400

    prevent searchform id from appearing twice in one page by relyiing on class

--- searchform.php
+++ searchform.php
@@ -17,7 +17,7 @@ $prefix = 'searchform-';
 
 ?>
 
-<form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+<form role="search" method="get" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label>
 		<span class="screen-reader-text"><?php _e( 'Search for:', 'page-builder-framework' ); ?></span>
 		<input type="search" id="<?php echo $prefix . (int) $wpbf_search_form_id++; ?>" name="s" value="" placeholder="<?php echo esc_attr( apply_filters( 'wpbf_search_placeholder', __( 'Search &hellip;', 'page-builder-framework' ) ) ); ?>" title="<?php echo esc_attr( apply_filters( 'wpbf_search_title', __( 'Press enter to search', 'page-builder-framework' ) ) ); ?>" />
