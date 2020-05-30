63794586020fe682946f73103878508d1f48394f
mapsteps <connect@mapsteps.com>
2020-05-30 11:30:08 +0200

    Fixed: Navigation background color overrides sticky header background color

--- inc/customizer/styles.php
+++ inc/customizer/styles.php
@@ -1847,7 +1847,7 @@ if ( $menu_padding ) {
 
 if ( $menu_bg_color ) {
 
-	echo '.wpbf-navigation:not(.wpbf-navigation-transparent) {';
+	echo '.wpbf-navigation:not(.wpbf-navigation-transparent):not(.wpbf-navigation-active) {';
 	echo sprintf( 'background-color: %s;', esc_attr( $menu_bg_color ) );
 	echo '}';
 
