7dd68038b0784a0e933ed29e695312ff5f84f2a1
mapsteps <connect@mapsteps.com>
2020-07-06 17:11:20 +0200

    restructured SCSS files

--- inc/template-parts/logo/logo.php
+++ inc/template-parts/logo/logo.php
@@ -23,7 +23,7 @@ if ( has_custom_logo() ) {
 
 	echo '<div class="wpbf-logo"' . $menu_active_logo . ' itemscope="itemscope" itemtype="https://schema.org/Organization">';
 	do_action( 'wpbf_before_logo' );
-	echo '<a class="wpbf-remove-font-size" href="' . esc_url( $menu_logo_url ) . '" itemprop="url">';
+	echo '<a href="' . esc_url( $menu_logo_url ) . '" itemprop="url">';
 	echo '<img src="' . esc_url( $custom_logo_url ) . '" alt="' . esc_attr( $menu_alt_tag ) . '" title="' . esc_attr( $menu_title_tag ) . '" itemprop="logo" />';
 	echo '</a>';
 	do_action( 'wpbf_after_logo' );
