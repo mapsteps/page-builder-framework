264cc35df53eef702f153dcbca11556eaa34e8fd
mapsteps <connect@mapsteps.com>
2020-07-14 10:03:43 +0200

    Fixed: PHP notice in logo.php & logo-mobile.php

--- inc/template-parts/logo/logo-mobile.php
+++ inc/template-parts/logo/logo-mobile.php
@@ -8,16 +8,16 @@
 
 defined( 'ABSPATH' ) || die( "Can't access directly" );
 
-$menu_alt_tag    = get_theme_mod( 'menu_logo_alt', get_bloginfo( 'name' ) );
-$menu_title_tag  = get_theme_mod( 'menu_logo_title', get_bloginfo( 'name' ) );
-$menu_logo_url   = get_theme_mod( 'menu_logo_url', home_url() );
-$custom_logo_id  = get_theme_mod( 'custom_logo' );
-$custom_logo_url = wp_get_attachment_image_src( $custom_logo_id, 'full' );
-$custom_logo_url = apply_filters( 'wpbf_logo_mobile', $custom_logo_url[0] );
-$tagline         = get_bloginfo( 'description' );
-$tagline_toggle  = get_theme_mod( 'menu_logo_description_mobile' );
+$menu_logo_url = get_theme_mod( 'menu_logo_url', home_url() );
+$menu_logo_url = is_customize_preview() ? site_url() : $menu_logo_url;
 
 if ( has_custom_logo() ) {
+
+	$menu_alt_tag    = get_theme_mod( 'menu_logo_alt', get_bloginfo( 'name' ) );
+	$menu_title_tag  = get_theme_mod( 'menu_logo_title', get_bloginfo( 'name' ) );
+	$custom_logo_id  = get_theme_mod( 'custom_logo' );
+	$custom_logo_url = wp_get_attachment_image_src( $custom_logo_id, 'full' );
+	$custom_logo_url = apply_filters( 'wpbf_logo_mobile', $custom_logo_url[0] );
 
 	echo '<div class="wpbf-mobile-logo" itemscope="itemscope" itemtype="https://schema.org/Organization">';
 	do_action( 'wpbf_before_mobile_logo' );
@@ -28,6 +28,9 @@ if ( has_custom_logo() ) {
 	echo '</div>';
 
 } else {
+
+	$tagline        = get_bloginfo( 'description' );
+	$tagline_toggle = get_theme_mod( 'menu_logo_description_mobile' );
 
 	echo '<div class="wpbf-mobile-logo" itemscope="itemscope" itemtype="https://schema.org/Organization">';
 	do_action( 'wpbf_before_mobile_logo' );
