8a0086a167b23a204836e5d99cde519c0b5c36dd
mapsteps <connect@mapsteps.com>
2020-11-23 01:42:45 +0100

    .

--- inc/settings/activation-notice.php
+++ inc/settings/activation-notice.php
@@ -19,17 +19,14 @@ global $wpbf_activation_notice_dismissal_nonce;
 			<h2>
 				<?php _e( 'Welcome to Page Builder Framework!', 'page-builder-framework' ); ?>
 			</h2>
-
 			<p>
 				<?php _e( 'Thank you for choosing Page Builder Framework! Please visit the theme settings page to get started.', 'page-builder-framework' ); ?>
 			</p>
-
 			<p>
 				<a href="<?php echo esc_url( admin_url( 'themes.php?page=wpbf-premium' ) ); ?>" class="button button-primary">
 					<?php _e( 'Get Started', 'page-builder-framework' ); ?>
 				</a>
 			</p>
-
 		</div>
 	</div>
 </div>
