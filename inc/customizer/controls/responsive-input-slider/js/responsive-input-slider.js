87f4d9e991f6622a92ce559e6f0abe21181c0508
Collins Agbonghama <collizo4sky@gmail.com>
2021-01-26 17:10:27 +0100

    Removed console.log

--- inc/customizer/controls/responsive-input-slider/js/responsive-input-slider.js
+++ inc/customizer/controls/responsive-input-slider/js/responsive-input-slider.js
@@ -2,14 +2,12 @@ jQuery(document).ready(function ($) {
 
     $('.wpbf-responsive-input-slider-wrap .customize-control-slider-value').change(function () {
 
-    	console.log('we here')
-
         var parent = $(this).parents('.wpbf-responsive-input-slider-wrap'),
             dbstore_cache = $('.wpbf-responsive-input-slider-db', parent),
             dbstore = dbstore_cache.val(),
             device_type = $(this).data('device-type');
 
-        dbstore = dbstore == '' ? {} : JSON.parse(dbstore);
+        dbstore = dbstore === '' ? {} : JSON.parse(dbstore);
 
         dbstore[device_type] = this.value;
 
