8f0c3f6ac27a487cf6594e2f98e018bf5170d256
mapsteps <connect@mapsteps.com>
2021-02-16 16:47:22 +0100

    autoformat

--- inc/customizer/controls/padding/control-padding.php
+++ inc/customizer/controls/padding/control-padding.php
@@ -12,18 +12,17 @@ class WPBF_Customize_Padding_Control extends Kirki_Control_Base {
 
 	public $type = 'wpbf-padding';
 
-        public function enqueue() {
+	public function enqueue() {
+		wp_enqueue_script( 'wpbf-padding', WPBF_THEME_URI . '/inc/customizer/controls/padding/js/padding.js', array( 'jquery' ), WPBF_VERSION, true );
+	}
 
-            wp_enqueue_script('wpbf-padding', WPBF_THEME_URI . '/inc/customizer/controls/padding/js/padding.js', array('jquery'), WPBF_VERSION, true);
-        }
-
-        public function render_content() {
+	public function render_content() {
 
 		$areas = array( 'top', 'right', 'bottom', 'left' );
 
-        $value_bucket = empty( $this->value() ) ? [] : json_decode( $this->value(), true );
+		$value_bucket = empty( $this->value() ) ? [] : json_decode( $this->value(), true );
 
-        echo '<div class="wpbf-padding-wrap">';
+		echo '<div class="wpbf-padding-wrap">';
 
 		?>
 
@@ -31,7 +30,7 @@ class WPBF_Customize_Padding_Control extends Kirki_Control_Base {
 
 		<?php foreach ( $areas as $area ) { ?>
 
-            <?php $saved_value = isset( $value_bucket[$area] ) ? $value_bucket[$area] : ''; ?>
+			<?php $saved_value = isset( $value_bucket[$area] ) ? $value_bucket[$area] : ''; ?>
 
 		<div class="wpbf-control-padding-<?php echo esc_attr( $area ); ?>">
 
@@ -48,12 +47,12 @@ class WPBF_Customize_Padding_Control extends Kirki_Control_Base {
 
 		<?php
 
-        printf(
-            '<input type="hidden" class="wpbf-padding-db" name="%s" value="%s" %s/>',
-            esc_attr( $this->id ), $this->value(), $this->get_link()
-        );
+		printf(
+			'<input type="hidden" class="wpbf-padding-db" name="%s" value="%s" %s/>',
+			esc_attr( $this->id ), $this->value(), $this->get_link()
+		);
 
-        echo '</div>';
+		echo '</div>';
 
 	}
 
@@ -67,12 +66,12 @@ class WPBF_Customize_Padding_Control extends Kirki_Control_Base {
  * @return array The updated controls.
  */
 add_filter(
-    'kirki_control_types',
-    function ( $controls ) {
+	'kirki_control_types',
+	function ( $controls ) {
 
-        $controls['padding_control'] = 'WPBF_Customize_Padding_Control';
+		$controls['padding_control'] = 'WPBF_Customize_Padding_Control';
 
-        return $controls;
+		return $controls;
 
-    }
+	}
 );
