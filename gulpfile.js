09e79ef0ca653cc43c4719f5cc2e834e74b2451b
Bagus <contactjavas@gmail.com>
2021-04-27 14:29:23 +0700

    simplify gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,8 @@ gulp.task('scripts_min', function () {
 });
 
 // Styles Task
-// Compile Main Styles
+
+// Compile main styles.
 gulp.task('styles', function(){
 	return gulp.src('assets/scss/style.scss')
 	.pipe(sass({outputStyle: 'compressed'}))
@@ -43,45 +44,15 @@ gulp.task('styles', function(){
 	.pipe(reload({ stream: true }))
 });
 
-// Compile Responsive Styles
-gulp.task('responsive_styles_min', function(){
-	return gulp.src('assets/scss/responsive.scss')
-	.pipe(sass({outputStyle: 'compressed'}))
-	.pipe(rename({ suffix: '-min' }))
-	.pipe(gulp.dest('css/min'))
-	.pipe(reload({ stream: true }))
-});
-
-// Compile RTL Styles
-gulp.task('rtl_styles_min', function(){
-	return gulp.src('assets/scss/rtl.scss')
-	.pipe(sass({outputStyle: 'compressed'}))
-	.pipe(rename({ suffix: '-min' }))
-	.pipe(gulp.dest('css/min'))
-	.pipe(reload({ stream: true }))
-});
-
-// Compile EDD Styles
-gulp.task('edd_styles_min', function(){
-	return gulp.src('assets/edd/scss/edd.scss')
-	.pipe(sass({outputStyle: 'compressed'})) 
-	.pipe(rename({ suffix: '-min' }))
-	.pipe(gulp.dest('css/min'))
-	.pipe(reload({ stream: true }))
-});
-
-// Compile LifterLMS Styles
-gulp.task('lifterlms_styles_min', function(){
-	return gulp.src('assets/lifterlms/scss/lifterlms.scss')
-	.pipe(sass({outputStyle: 'compressed'})) 
-	.pipe(rename({ suffix: '-min' }))
-	.pipe(gulp.dest('css/min'))
-	.pipe(reload({ stream: true }))
-});
-
-// Compile Woo Styles
-gulp.task('woo_styles_min', function(){
-	return gulp.src(['assets/woocommerce/scss/woocommerce-layout.scss', 'assets/woocommerce/scss/woocommerce.scss', 'assets/woocommerce/scss/woocommerce-smallscreen.scss'])
+// Compile partial styles.
+gulp.task('partial_styles', function(){
+	return gulp.src([
+		'assets/scss/*.scss',
+		'!assets/scss/style.scss',
+		'assets/edd/scss/*.scss',
+		'assets/lifterlms/scss/*.scss',
+		'assets/woocommerce/scss/*.scss'
+	])
 	.pipe(sass({outputStyle: 'compressed'}))
 	.pipe(rename({ suffix: '-min' }))
 	.pipe(gulp.dest('css/min'))
@@ -104,16 +75,29 @@ gulp.task('watch', function() {
 	gulp.watch(['assets/js/site.js', 'assets/js/mobile.js'], ['scripts_combine_min']);
 	gulp.watch(['assets/js/*.js', '!assets/js/site.js', '!assets/js/mobile.js'], ['scripts_min']);
 
-	gulp.watch('assets/scss/**/*.scss', ['styles']);
-	gulp.watch('assets/scss/**/*.scss', ['responsive_styles_min']);
-	gulp.watch('assets/scss/**/*.scss', ['rtl_styles_min']);
-	gulp.watch('assets/woocommerce/scss/**/*.scss', ['woo_styles_min']);
-	gulp.watch('assets/edd/scss/**/*.scss', ['edd_styles_min']);
-	gulp.watch('assets/lifterlms/scss/**/*.scss', ['lifterlms_styles_min']);
+	gulp.watch(['assets/scss/style.scss', 'assets/scss/**/*.scss'], ['styles']);
+	
+	gulp.watch(
+		[
+			'assets/scss/*.scss',
+			'!assets/scss/style.scss',
+			'assets/scss/**/*.scss',
+			
+			'assets/edd/scss/*.scss',
+			'assets/edd/scss/**/*.scss',
+			
+			'assets/lifterlms/scss/*.scss',
+			'assets/lifterlms/scss/**/*.scss',
+			
+			'assets/woocommerce/scss/*.scss',
+			'assets/woocommerce/scss/**/*.scss'
+		],
+		['partial_styles']
+	);
 
 	// browserSync
 	gulp.watch('**/*.php').on('change', reload);
 })
 
 // Gulp
-gulp.task('default', ['scripts_combine_min', 'scripts_min', 'styles', 'responsive_styles_min', 'rtl_styles_min', 'edd_styles_min', 'lifterlms_styles_min', 'woo_styles_min', 'watch', 'serve']);
+gulp.task('default', ['scripts_combine_min', 'scripts_min', 'styles', 'partial_styles', 'watch', 'serve']);
