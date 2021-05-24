9342cbb7dae8e70b7e39e77f556a191910e691f6
Bagus <contactjavas@gmail.com>
2021-05-24 23:35:08 +0700

    only use specific block styles instead of the whole css framework

--- inc/integration/gutenberg/build/wpbf-block-editor.asset.php
+++ inc/integration/gutenberg/build/wpbf-block-editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '8f698cbf040fcb06121dc211491fe789');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '4b1bd12781300f3cef4d58bed3f382da');
