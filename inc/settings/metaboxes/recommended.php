ef3c6c98162d1c476f3c698e45285cd9bbb633ef
mapsteps <connect@mapsteps.com>
2021-05-28 15:11:37 +0200

    remove Toolset from recommended plugins for now

--- inc/settings/metaboxes/recommended.php
+++ inc/settings/metaboxes/recommended.php
@@ -63,14 +63,14 @@ defined( 'ABSPATH' ) || die( "Can't access directly" );
 					'repo'        => true,
 					'constant'    => 'WEGLOT_VERSION',
 				),
-				array(
-					'title'       => __( 'Toolset', 'page-builder-framework' ),
-					'description' => __( 'A set of plugins to help you build dynamic, advanced websites without programming.', 'page-builder-framework' ),
-					'banner'      => WPBF_THEME_URI .'/img/toolset-banner.png',
-					'link'        => 'https://toolset.com/',
-					'repo'        => false,
-					'constant'    => 'WPBF_PREMIUM_VERSION', // missing!
-				),
+				// array(
+				// 	'title'       => __( 'Toolset', 'page-builder-framework' ),
+				// 	'description' => __( 'A set of plugins to help you build dynamic, advanced websites without programming.', 'page-builder-framework' ),
+				// 	'banner'      => WPBF_THEME_URI .'/img/toolset-banner.png',
+				// 	'link'        => 'https://toolset.com/',
+				// 	'repo'        => false,
+				// 	'constant'    => 'WPBF_PREMIUM_VERSION', // missing!
+				// ),
 			);
 
 			foreach ( $recommended_plugins as $recommended_plugin ) {
