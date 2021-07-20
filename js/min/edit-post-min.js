92084ccdaba03a4ebf9c8516fc57d829dd8f96c6
mapsteps <connect@mapsteps.com>
2021-07-20 14:24:28 +0200

    Tweak: Gutenberg support for sidebar blocks

--- js/min/edit-post-min.js
+++ js/min/edit-post-min.js
@@ -1,1 +1,1 @@
-"use strict";!function(e){var t=document.querySelector(".wpbf-layout-custom-width-field-wrapper");e(".wpbf-layout-option").on("change",function(){document.querySelector("#layout-custom-width").checked?t.classList.remove("wpbf-is-hidden"):t.classList.add("wpbf-is-hidden")})}(jQuery);
+"use strict";!function(e){var t=document.querySelector(".wpbf-layout-custom-width-field-wrapper");function o(){document.querySelector("#layout-custom-width").checked?t.classList.remove("wpbf-is-hidden"):t.classList.add("wpbf-is-hidden")}e(".wpbf-layout-option").on("change",o)}(jQuery);
