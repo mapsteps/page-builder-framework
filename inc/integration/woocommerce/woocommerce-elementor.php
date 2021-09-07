847e1121c936b38df676454851873bb03654cb51
mapsteps <connect@mapsteps.com>
2021-09-07 17:09:27 +0200

    this should be a bit safer to do as Elementor targets .products specifically when overriding styles

--- inc/integration/woocommerce/woocommerce-elementor.php
+++ inc/integration/woocommerce/woocommerce-elementor.php
@@ -14,6 +14,6 @@ defined( 'ABSPATH' ) || die( "Can't access directly" );
  * https://github.com/elementor/elementor/issues/16057
  */
 function wpbf_woo_elementor_grid_fix() {
-	echo '<style id="wpbf-elementor-woocommerce-product-loop-fix">.wpbf-grid.elementor-grid { display:  flex }</style>';
+	echo '<style id="wpbf-elementor-woocommerce-product-loop-fix">.products.elementor-grid { display:  flex }</style>';
 }
 add_action( 'wp_head', 'wpbf_woo_elementor_grid_fix', 999 );
