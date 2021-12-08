85c2c9d5ac7d2ba9eba3fb7894612a7b341d9c78
mapsteps <connect@mapsteps.com>
2021-12-08 13:11:08 +0100

    updated kirki

--- assets/kirki/packages/composer/installed.php
+++ assets/kirki/packages/composer/installed.php
@@ -281,12 +281,12 @@
             'dev_requirement' => false,
         ),
         'kirki-framework/field-typography' => array(
-            'pretty_version' => 'v1.0.3',
-            'version' => '1.0.3.0',
+            'pretty_version' => 'v1.0.4',
+            'version' => '1.0.4.0',
             'type' => 'library',
             'install_path' => __DIR__ . '/../kirki-framework/field-typography',
             'aliases' => array(),
-            'reference' => '49d6e04b3040042fce0751335f7d4ababc15af53',
+            'reference' => 'eb1d797ec4dd2cf24f18121de0ad8b599a5ec2da',
             'dev_requirement' => false,
         ),
         'kirki-framework/googlefonts' => array(
