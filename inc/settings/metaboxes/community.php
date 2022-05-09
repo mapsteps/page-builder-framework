6e30da4a8bf7b06073f64223b243369b2f93d28a
mapsteps <connect@mapsteps.com>
2022-05-09 11:43:47 +0200

    1700

--- inc/settings/metaboxes/community.php
+++ inc/settings/metaboxes/community.php
@@ -12,7 +12,7 @@ defined( 'ABSPATH' ) || die( "Can't access directly" );
 	<h2><?php _e( 'Join the Community', 'page-builder-framework' ); ?></h2>
 	<div class="heatbox-content">
 		<p>
-			<?php _e( 'Join the community and meet 1500+ Page Builder Framework users in our Facebook group.', 'page-builder-framework' ); ?>
+			<?php _e( 'Join the community and meet 1700+ Page Builder Framework users in our Facebook group.', 'page-builder-framework' ); ?>
 		</p>
 		<a href="https://www.facebook.com/groups/wpagebuilderframework/" target="_blank" class="button">
 			<?php _e( 'Join the Community', 'page-builder-framework' ); ?>
