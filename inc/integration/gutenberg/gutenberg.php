761e1391131d96f7f0855cd18cc61996e590db8b
Bagus <25834188+contactjavas@users.noreply.github.com>
2022-05-27 19:24:16 +0700

    remove blocks from PBF (see description)
    WP makes it possible to register_block_type using json file inside the theme. But they don't allow the theme developer to upload their theme if it contains register_block_type.
    
    But we're not going back with register_block_type_from_metadata because the best practice is to use register_block_type. See the comment in https://developer.wordpress.org/reference/functions/register_block_type_from_metadata/
    
    So for now, removing the blocks (currently only notice(s) blocks) is the best idea we can think of.
    
    For existing users who write their post using PBF block, the output on the frontend is still fine. We keep the styling for compatibility purpose.  And in the admin area (editor), they can keep the content by converting notice block to HTML block.

--- inc/integration/gutenberg/gutenberg.php
+++ inc/integration/gutenberg/gutenberg.php
@@ -19,54 +19,8 @@ function wpbf_gutenberg_theme_setup() {
 	// Add support for wide aligned elements.
 	add_theme_support( 'align-wide' );
 
-	// We only support blocks from WP version 5.9 and up.
-	if ( version_compare( get_bloginfo( 'version' ), '5.9', '<' ) ) {
-		return;
-	}
-
-	// Register block categories.
-	add_filter( 'block_categories_all', 'wpbf_register_blocks_category', 10, 2 );
-
-	// Register PBF's blocks.
-	wpbf_register_blocks();
-
 }
 add_action( 'after_setup_theme', 'wpbf_gutenberg_theme_setup' );
-
-/**
- * Register blocks category.
- *
- * @param array                    $categories Array of categories for block types.
- * @param \WP_Block_Editor_Context $block_editor_context The current block editor context.
- *
- * @return array The modified block categories.
- */
-function wpbf_register_blocks_category( $categories, $block_editor_context ) {
-
-	return array_merge(
-		$categories,
-		array(
-			array(
-				'slug'  => 'wpbf',
-				'title' => __( 'Page Builder Framework', 'page-builder-framework' ),
-			),
-		)
-	);
-
-}
-
-/**
- * Register blocks.
- */
-function wpbf_register_blocks() {
-
-	$scan = glob( __DIR__ . '/blocks/*/block.php' );
-
-	foreach ( $scan as $path ) {
-		require_once $path;
-	}
-
-}
 
 /**
  * Add editor styles.
@@ -79,23 +33,6 @@ function wpbf_gutenberg_block_editor_assets() {
 		wp_enqueue_style( 'wpbf-gutenberg-style', get_template_directory_uri() . '/css/block-editor-styles.css', '', WPBF_VERSION );
 		wp_add_inline_style( 'wpbf-gutenberg-style', $inline_styles );
 	}
-
-	// We only support blocks from WP version 5.9 and up.
-	if ( version_compare( get_bloginfo( 'version' ), '5.9', '<' ) ) {
-		return;
-	}
-
-	$dependencies = array( 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives' );
-
-	wp_enqueue_style( 'wpbf-block-editor', WPBF_THEME_URI . '/inc/integration/gutenberg/build/wpbf-block-editor.css', array(), WPBF_VERSION );
-
-	wp_enqueue_script( 'wpbf-block-editor', WPBF_THEME_URI . '/inc/integration/gutenberg/build/wpbf-block-editor.js', $dependencies, WPBF_VERSION, true );
-
-	wp_add_inline_script(
-		'wp-block-editor',
-		'var wpbfBlocks = {};',
-		'before'
-	);
 
 }
 add_action( 'enqueue_block_editor_assets', 'wpbf_gutenberg_block_editor_assets' );
--- inc/integration/gutenberg/src/wpbf-block-editor.js
+++
@@ -1,5 +0,0 @@
-import "./wpbf-block-editor.scss";
-
-import "./blocks/notices/editor.js";
-import "./blocks/notice/editor.js";
-
