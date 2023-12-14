3d110b99e51a9d9af7582348f7c535b0dcce2488
Bagus <25834188+contactjavas@users.noreply.github.com>
2023-12-14 16:10:04 +0700

    autoformat that doesn't hurt

--- inc/customizer/customizer-functions.php
+++ inc/customizer/customizer-functions.php
@@ -13,10 +13,10 @@ if ( ! function_exists( 'wpbf_get_theme_mod_value' ) ) {
 	/**
 	 * Helper function to get theme_mod array values by key.
 	 *
-	 * @param array $array The decoded theme_mod array.
-	 * @param string $key The array key.
+	 * @param array   $array The decoded theme_mod array.
+	 * @param string  $key The array key.
 	 * @param boolean $default The default to check against.
-	 * @param booleon $print_default Wether the default value should be returned.
+	 * @param boolean $print_default Whether the default value should be returned.
 	 *
 	 * @return mixed The key value.
 	 */
@@ -36,7 +36,7 @@ if ( ! function_exists( 'wpbf_get_theme_mod_value' ) ) {
 		}
 
 		// Get & set the value by key if we have one.
-		$value = isset( $array[$key] ) ? $array[$key] : $value;
+		$value = isset( $array[ $key ] ) ? $array[ $key ] : $value;
 
 		// If we don't want to return a default and the saved
 		// value matches default, we set value back to false.
@@ -58,10 +58,10 @@ function wpbf_adjust_customizer_responsive_sizes() {
 	$medium_breakpoint = function_exists( 'wpbf_breakpoint_medium' ) ? wpbf_breakpoint_medium() : 768;
 	$mobile_breakpoint = function_exists( 'wpbf_breakpoint_mobile' ) ? wpbf_breakpoint_mobile() : 480;
 
-	$tablet_margin_left = -$medium_breakpoint / 2 . 'px';
+	$tablet_margin_left = - $medium_breakpoint / 2 . 'px';
 	$tablet_width       = $medium_breakpoint . 'px';
 
-	$mobile_margin_left = -$mobile_breakpoint / 2 . 'px';
+	$mobile_margin_left = - $mobile_breakpoint / 2 . 'px';
 	$mobile_width       = $mobile_breakpoint . 'px';
 
 	?>
@@ -71,6 +71,7 @@ function wpbf_adjust_customizer_responsive_sizes() {
 			margin-left: <?php echo esc_attr( $tablet_margin_left ); ?>;
 			width: <?php echo esc_attr( $tablet_width ); ?>;
 		}
+
 		.wp-customizer .preview-mobile .wp-full-overlay-main {
 			margin-left: <?php echo esc_attr( $mobile_margin_left ); ?>;
 			width: <?php echo esc_attr( $mobile_width ); ?>;
@@ -79,8 +80,8 @@ function wpbf_adjust_customizer_responsive_sizes() {
 	</style>
 
 	<?php
-
 }
+
 add_action( 'customize_controls_print_styles', 'wpbf_adjust_customizer_responsive_sizes' );
 
 /**
@@ -92,13 +93,13 @@ add_action( 'customize_controls_print_styles', 'wpbf_adjust_customizer_responsiv
  */
 function wpbf_minify_css( $css ) {
 
-	// Remove Comments
+	// Remove Comments.
 	$css = preg_replace( '!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $css );
-	// Remove space after colons
+	// Remove space after colons.
 	$css = str_replace( ': ', ':', $css );
 	$css = str_replace( ' {', '{', $css );
 	$css = str_replace( ', ', ',', $css );
-	// Remove whitespace
+	// Remove whitespace.
 	$css = str_replace( array( "\r\n", "\r", "\n", "\t", '  ', '    ', '    ' ), '', $css );
 
 	return $css;
@@ -112,6 +113,7 @@ function wpbf_generate_css() {
 
 	ob_start();
 	include get_template_directory() . '/inc/customizer/styles.php';
+
 	return wpbf_minify_css( ob_get_clean() );
 
 }
@@ -155,6 +157,7 @@ function wpbf_create_customizer_css_file() {
 	}
 
 }
+
 add_action( 'wp_loaded', 'wpbf_create_customizer_css_file' );
 
 /**
@@ -182,6 +185,7 @@ function wpbf_customizer_frontend_scripts() {
 	}
 
 }
+
 add_action( 'wp_enqueue_scripts', 'wpbf_customizer_frontend_scripts', 11 );
 
 /**
@@ -198,6 +202,7 @@ function wpbf_customizer_preview_css() {
 	echo '</style>';
 
 }
+
 add_action( 'wp_head', 'wpbf_customizer_preview_css', 999 );
 
 /**
@@ -205,9 +210,19 @@ add_action( 'wp_head', 'wpbf_customizer_preview_css', 999 );
  */
 function wpbf_customizer_preview_js() {
 
-	wp_enqueue_script( 'wpbf-postmessage', get_template_directory_uri() . '/inc/customizer/js/postmessage.js', array( 'jquery', 'customize-preview' ), WPBF_VERSION, true );
+	wp_enqueue_script(
+		'wpbf-postmessage',
+		get_template_directory_uri() . '/inc/customizer/js/postmessage.js',
+		array(
+			'jquery',
+			'customize-preview',
+		),
+		WPBF_VERSION,
+		true
+	);
 
 }
+
 add_action( 'customize_preview_init', 'wpbf_customizer_preview_js' );
 
 /**
@@ -222,6 +237,7 @@ function wpbf_customizer_scripts_styles() {
 	wp_enqueue_script( 'responsive-controls', get_template_directory_uri() . '/inc/customizer/js/responsive-controls.js', array( 'jquery' ), WPBF_VERSION, true );
 
 }
+
 add_action( 'customize_controls_print_styles', 'wpbf_customizer_scripts_styles' );
 
 // Stop here if WP_Customize_Control doesn't exist.
@@ -244,6 +260,7 @@ function wpbf_custom_controls_238290( $wp_customize ) {
 	require WPBF_THEME_DIR . '/inc/customizer/controls/responsive-input-slider/control-responsive-input-slider.php';
 
 }
+
 add_action( 'customize_register', 'wpbf_custom_controls_238290' );
 
 /**
@@ -255,27 +272,28 @@ add_action( 'customize_register', 'wpbf_custom_controls_238290' );
  */
 function wpbf_custom_default_fonts( $standard_fonts ) {
 
-    $standard_fonts = array();
+	$standard_fonts = array();
 
-    $standard_fonts['helvetica_neue'] = array(
-        'label'    => 'Helvetica Neue',
-        'variants' => array( 'regular', 'italic', '700', '700italic' ),
-        'stack'    => '"Helvetica Neue", Helvetica, Arial, sans-serif',
-    );
+	$standard_fonts['helvetica_neue'] = array(
+		'label'    => 'Helvetica Neue',
+		'variants' => array( 'regular', 'italic', '700', '700italic' ),
+		'stack'    => '"Helvetica Neue", Helvetica, Arial, sans-serif',
+	);
 
-    $standard_fonts['helvetica'] = array(
-        'label'    => 'Helvetica',
-        'variants' => array( 'regular', 'italic', '700', '700italic' ),
-        'stack'    => 'Helvetica, Arial, sans-serif',
-    );
+	$standard_fonts['helvetica'] = array(
+		'label'    => 'Helvetica',
+		'variants' => array( 'regular', 'italic', '700', '700italic' ),
+		'stack'    => 'Helvetica, Arial, sans-serif',
+	);
 
-    $standard_fonts['arial'] = array(
-        'label'    => 'Arial',
-        'variants' => array( 'regular', 'italic', '700', '700italic' ),
-        'stack'    => 'Arial, Helvetica, sans-serif',
-    );
+	$standard_fonts['arial'] = array(
+		'label'    => 'Arial',
+		'variants' => array( 'regular', 'italic', '700', '700italic' ),
+		'stack'    => 'Arial, Helvetica, sans-serif',
+	);
 
-    return $standard_fonts;
+	return $standard_fonts;
 
 }
+
 add_filter( 'kirki/fonts/standard_fonts', 'wpbf_custom_default_fonts', 0 );
