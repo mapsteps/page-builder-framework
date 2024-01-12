f27aef013b288354707545900eef6f3a81dd104d
Bagus <25834188+contactjavas@users.noreply.github.com>
2024-01-12 17:00:40 +0700

    fix missing WpbfTheme

--- assets/js/site-jquery.js
+++ assets/js/site-jquery.js
@@ -3,8 +3,11 @@ import setupMobileMenu from "./setup/mobile-menu-jquery";
 import setupSite from "./setup/site-general-jquery";
 
 /** @type {LegacyWpbfTheme} */
-const WpbfTheme = {};
+const WpbfTheme = {
+    site: setupSite(jQuery),
+};
 
-WpbfTheme.site = setupSite(jQuery);
+window["WpbfTheme"] = WpbfTheme;
+
 setupDesktopMenu(jQuery);
 setupMobileMenu(jQuery);
