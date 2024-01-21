c5c5d00e95e08018940e80a8b1235964c2efeff1
Bagus <25834188+contactjavas@users.noreply.github.com>
2024-01-21 07:24:59 +0700

    remove un-used file

--- Customizer/Controls/Base/src/interfaces.ts
+++
@@ -1,7 +0,0 @@
-import {Customize} from "@wordpress/customize-browser/Customize";
-import {Control} from "@wordpress/customize-browser/Control";
-
-export interface WPCustomize extends Customize {
-	Control: Control;
-	wpbfDynamicControl?: Control;
-}
