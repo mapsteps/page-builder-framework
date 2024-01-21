31d2d95780178f3d4cd3acb4624513dfa262ca81
Bagus <25834188+contactjavas@users.noreply.github.com>
2024-01-21 07:23:01 +0700

    type-safety

--- Customizer/Controls/Base/src/base-control.ts
+++ Customizer/Controls/Base/src/base-control.ts
@@ -1,12 +1,20 @@
 import {Customize} from "wordpress__customize-browser/Customize";
-// Import wp.hooks from '@wordpress/hooks'
 import hooks from "@wordpress/hooks";
 import jQuery from "jquery";
 import _ from "lodash";
+import {Control, Control_Params} from "wordpress__customize-browser/Control";
+import {Element} from "wordpress__customize-browser/Element";
 
 declare var wp: {
 	customize: Customize;
 	hooks: typeof hooks;
+};
+
+export interface WpbfCustomizeControl extends Control {
+	_setUpSettingRootLinks: () => void;
+	_setUpSettingPropertyLinks: () => void;
+	initWpbfControl: (control?: Control) => void;
+	actuallyEmbed: () => void;
 }
 
 /**
@@ -23,44 +31,46 @@ declare var wp: {
 	'use strict';
 
 	wp.customize.wpbfDynamicControl = wp.customize.Control.extend({
-		initialize: function (id: string, options: Record<string, any>) {
-			let control = this;
-			let args = options || {};
+		initialize: function (id: string, params: Control_Params) {
+			const control: Control = this as WpbfCustomizeControl;
 
-			args.params = args.params || {};
-
-			if (!args.params.type) {
-				args.params.type = 'wpbf-generic';
+			if (!params.type) {
+				params.type = 'wpbf-generic';
 			}
 
 			let className = '';
 
-			if (args.content) {
-				let splits = args.content.split('class="');
+			if (params.content) {
+				let splits = params.content.split('class="');
 				splits = splits[1].split('"');
 				className = splits[0];
 			} else {
-				className = 'customize-control customize-control-' + args.params.type;
+				className = 'customize-control customize-control-' + params.type;
 			}
 
-			// Hijack the container to add wrapper_attrs.
-			args.params.content = jQuery("<li></li>");
-			args.params.content.attr('id', 'customize-control-' + id.replace(/]/g, '').replace(/\[/g, '-'));
-			args.params.content.attr('class', className);
+			if (params.content) {
+				const $content = jQuery(params.content);
+				$content.attr('id', 'customize-control-' + id.replace(/]/g, '').replace(/\[/g, '-'));
+				$content.attr('class', className);
 
-			const wrapperAttrs = args.params['wrapper_attrs'] || {};
+				const wrapperAttrs = params['wrapper_attrs'] ?? {};
 
-			_.each(wrapperAttrs, function (val: any, key: string) {
-				if ('class' === key) {
-					val = val.replace('{default_class}', className);
-				}
+				_.each(wrapperAttrs, function (val: any, key: string) {
+					if ('class' === key) {
+						val = val.replace('{default_class}', className);
+					}
 
-				args.params.content.attr(key, val);
-			});
+					$content.attr(key, val);
+				});
+
+				// Hijack the container to add wrapper_attrs.
+				params.content = $content.prop('outerHTML');
+			}
+
 
 			control.propertyElements = [];
-			wp.customize.Control.prototype.initialize.call(control, id, args);
-			wp.hooks.doAction('wpbf.dynamicControl.init.after', id, control, args);
+			wp.customize.Control.prototype.initialize.call(control, id, params);
+			wp.hooks.doAction('wpbf.dynamicControl.init.after', id, control, params);
 		},
 
 		/**
@@ -68,19 +78,17 @@ declare var wp: {
 		 *
 		 * This is copied from wp.customize.Control.prototype.initialize(). It
 		 * should be changed in Core to be applied once the control is embedded.
-		 *
-		 * @private
-		 * @returns {void}
 		 */
 		_setUpSettingRootLinks: function () {
-			const control = this;
+			const control = this as WpbfCustomizeControl;
 			const nodes = control.container.find('[data-customize-setting-link]');
 
 			nodes.each(function () {
 				const node = jQuery(this);
 
 				wp.customize(node.data('customizeSettingLink'), function (setting) {
-					const element = new wp.customize.Element(node);
+					const element = wp.customize.Element(node);
+
 					control.elements.push(element);
 					element.sync(setting);
 					element.set(setting());
@@ -90,12 +98,9 @@ declare var wp: {
 
 		/**
 		 * Add bidirectional data binding links between inputs and the setting properties.
-		 *
-		 * @private
-		 * @returns {void}
 		 */
 		_setUpSettingPropertyLinks: function () {
-			const control = this;
+			const control = this as WpbfCustomizeControl;
 			let nodes;
 
 			if (!control.setting) {
@@ -106,14 +111,23 @@ declare var wp: {
 
 			nodes.each(function () {
 				const node = jQuery(this);
-				let element;
+				let element: Element;
 				let propertyName = node.data('customizeSettingPropertyLink');
 
-				element = new wp.customize.Element(node);
+				element = wp.customize.Element(node);
 				control.propertyElements.push(element);
+
+				if (!control.setting || typeof control.setting !== 'function') {
+					return;
+				}
+
 				element.set(control.setting()[propertyName]);
 
 				element.bind(function (newPropertyValue) {
+					if (!control.setting || typeof control.setting !== 'function') {
+						return;
+					}
+
 					let newSetting = control.setting();
 
 					if (newPropertyValue === newSetting[propertyName]) {
@@ -124,6 +138,7 @@ declare var wp: {
 					newSetting[propertyName] = newPropertyValue;
 					control.setting.set(newSetting);
 				});
+
 				control.setting.bind(function (newValue) {
 					if (newValue[propertyName] !== element.get()) {
 						element.set(newValue[propertyName]);
@@ -136,7 +151,7 @@ declare var wp: {
 		 * @inheritdoc
 		 */
 		ready: function () {
-			const control = this;
+			const control = this as WpbfCustomizeControl;
 
 			control._setUpSettingRootLinks();
 			control._setUpSettingPropertyLinks();
@@ -144,7 +159,7 @@ declare var wp: {
 			wp.customize.Control.prototype.ready.call(control);
 
 			control.deferred.embedded.done(function () {
-				control.initKirkiControl();
+				control.initWpbfControl();
 				wp.hooks.doAction('wpbf.dynamicControl.ready.deferred.embedded.done', control);
 			});
 			wp.hooks.doAction('wpbf.dynamicControl.ready.after', control);
@@ -156,11 +171,9 @@ declare var wp: {
 		 * Override the embed() method to do nothing,
 		 * so that the control isn't embedded on load,
 		 * unless the containing section is already expanded.
-		 *
-		 * @returns {void}
 		 */
 		embed: function () {
-			const control = this;
+			const control = this as WpbfCustomizeControl;
 			let sectionId = control.section();
 
 			if (!sectionId) {
@@ -186,11 +199,9 @@ declare var wp: {
 		 *
 		 * This function is called in Section.onChangeExpanded() so the control
 		 * will only get embedded when the Section is first expanded.
-		 *
-		 * @returns {void}
 		 */
 		actuallyEmbed: function () {
-			const control = this;
+			const control = this as WpbfCustomizeControl;
 
 			if ('resolved' === control.deferred.embedded.state()) {
 				return;
@@ -202,12 +213,10 @@ declare var wp: {
 
 		/**
 		 * This is not working with autofocus.
-		 *
-		 * @param {object} [args] Args.
-		 * @returns {void}
 		 */
-		focus: function (args) {
-			const control = this;
+		focus: function (args: Record<string, any>) {
+			const control = this as WpbfCustomizeControl;
+
 			control.actuallyEmbed();
 			wp.customize.Control.prototype.focus.call(control, args);
 			wp.hooks.doAction('wpbf.dynamicControl.focus.after', control);
@@ -215,32 +224,29 @@ declare var wp: {
 
 		/**
 		 * Additional actions that run on ready.
-		 *
-		 * @param {object} control - The control object. If undefined fallsback to the this.
-		 * @returns {void}
 		 */
-		initKirkiControl: function (control) {
-			control = control || this;
-			wp.hooks.doAction('wpbf.dynamicControl.initKirkiControl', this);
+		initWpbfControl: function (control?: Control) {
+			control = control ?? this as WpbfCustomizeControl;
+
+			wp.hooks.doAction('wpbf.dynamicControl.initWpbfControl', this);
 
 			// Save the value
 			control.container.on('change keyup paste click', 'input', function () {
-				control.setting.set(jQuery(this).val());
+				if (!control?.setting || typeof control?.setting !== 'function') {
+					return;
+				}
+
+				control?.setting?.set(jQuery(this).val());
 			});
 		}
 	});
 }());
 
 (function (api) {
-
 	/**
 	 * Set the value and trigger all bound callbacks.
-	 *
-	 * @since 1.0
-	 * @param {object} to - New value.
-	 * @returns {Object} - this
 	 */
-	api.Value.prototype.set = function (to) {
+	api.Value.prototype.set = function (to: any) {
 		const from = this._value;
 
 		to = this._setter.apply(this, arguments);
@@ -251,26 +257,24 @@ declare var wp: {
 			return this;
 		}
 
-
 		/**
-		 * Parent settings.
 		 * This was brought from Kirki.
 		 * But this is too much for now.
 		 *
 		 * Start Kirki mod.
 		 */
-		// let parentSetting;
-		// let newVal;
-		// if (this.id && api.control(this.id) && api.control(this.id).params && api.control(this.id).params.parent_setting) {
-		// 	parentSetting = api.control(this.id).params.parent_setting;
-		// 	newVal = {};
-		// 	newVal[this.id.replace(parentSetting + '[', '').replace(']', '')] = to;
-		// 	api.control(parentSetting).setting.set(jQuery.extend({}, api.control(parentSetting).setting._value, newVal));
-		// }
+		/*
+		let parentSetting;
+		let newVal;
+		if (this.id && api.control(this.id) && api.control(this.id).params && api.control(this.id).params.parent_setting) {
+			parentSetting = api.control(this.id).params.parent_setting;
+			newVal = {};
+			newVal[this.id.replace(parentSetting + '[', '').replace(']', '')] = to;
+			api.control(parentSetting).setting.set(jQuery.extend({}, api.control(parentSetting).setting._value, newVal));
+		}
+		*/
 
-		/**
-		 * End Kirki mod.
-		 */
+		// End Kirki mod.
 
 		this._value = to;
 		this._dirty = true;
@@ -285,16 +289,20 @@ declare var wp: {
 	 */
 	api.Value.prototype.get = function () {
 
-		// This was brought from Kirki.
-		// But this is too much for now.
-		//
-		// Start Kirki mod.
-		// let parentSetting;
-		//
-		// if (this.id && api.control(this.id) && api.control(this.id).params && api.control(this.id).params.parent_setting) {
-		// 	parentSetting = api.control(this.id).params.parent_setting;
-		// 	return api.control(parentSetting).setting.get()[this.id.replace(parentSetting + '[', '').replace(']', '')];
-		// }
+		/**
+		 * This was brought from Kirki.
+		 * But this is too much for now.
+		 *
+		 * Start Kirki mod.
+		 */
+		/*
+		let parentSetting;
+
+		if (this.id && api.control(this.id) && api.control(this.id).params && api.control(this.id).params.parent_setting) {
+			parentSetting = api.control(this.id).params.parent_setting;
+			return api.control(parentSetting).setting.get()[this.id.replace(parentSetting + '[', '').replace(']', '')];
+		}
+		*/
 		// End Kirki mod.
 
 		return this._value;
