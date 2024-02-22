ad5ee380a42f723058253fa8e2aa9452ecadaadd
Bagus <25834188+contactjavas@users.noreply.github.com>
2024-02-22 09:39:56 +0700

    update composer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'mapsteps/aura',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'a36b31fc02518abc400657f9c03f0f31374cf98a',
+        'reference' => '7ccc1619581696e29c0c2c34dcfe5a6ced39b2c2',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'mapsteps/aura' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'a36b31fc02518abc400657f9c03f0f31374cf98a',
+            'reference' => '7ccc1619581696e29c0c2c34dcfe5a6ced39b2c2',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
