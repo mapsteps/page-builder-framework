6cdcd51e689cbed1bb0485e0a0871ec296cbdf20
Bagus <25834188+contactjavas@users.noreply.github.com>
2024-03-01 15:28:04 +0700

    convert controls in "Theme Buttons" section

--- inc/customizer/settings/settings-general.php
+++ inc/customizer/settings/settings-general.php
@@ -7,7 +7,6 @@
  */
 
 use Kirki\Field\Toggle;
-use Kirki\Pro\Field\Divider;
 use Kirki\Pro\Field\Headline;
 use Kirki\Pro\Field\HeadlineToggle;
 
@@ -732,256 +731,236 @@ wpbf_customizer_field()
 	->addToSection( 'wpbf_button_options' );
 
 // Background color.
-Kirki::add_field( 'wpbf', array(
-	'type'      => 'color',
-	'settings'  => 'button_bg_color',
-	'label'     => __( 'Background Color', 'page-builder-framework' ),
-	'section'   => 'wpbf_button_options',
-	'priority'  => 1,
-	'transport' => 'postMessage',
-	'choices'   => array(
-		'alpha' => true,
-	),
-) );
+wpbf_customizer_field()
+	->id( 'button_bg_color' )
+	->type( 'color' )
+	->label( __( 'Background Color', 'page-builder-framework' ) )
+	->priority( 1 )
+	->transport( 'postMessage' )
+	->properties( array(
+		'mode' => 'alpha',
+	) )
+	->addToSection( 'wpbf_button_options' );
 
 // Background color alt.
-Kirki::add_field( 'wpbf', array(
-	'type'      => 'color',
-	'settings'  => 'button_bg_color_alt',
-	'label'     => __( 'Hover', 'page-builder-framework' ),
-	'section'   => 'wpbf_button_options',
-	'priority'  => 1,
-	'transport' => 'postMessage',
-	'choices'   => array(
-		'alpha' => true,
-	),
-) );
+wpbf_customizer_field()
+	->id( 'button_bg_color_alt' )
+	->type( 'color' )
+	->label( __( 'Hover', 'page-builder-framework' ) )
+	->priority( 1 )
+	->transport( 'postMessage' )
+	->properties( array(
+		'mode' => 'alpha',
+	) )
+	->addToSection( 'wpbf_button_options' );
 
 // Text color.
-Kirki::add_field( 'wpbf', array(
-	'type'      => 'color',
-	'settings'  => 'button_text_color',
-	'label'     => __( 'Font Color', 'page-builder-framework' ),
-	'section'   => 'wpbf_button_options',
-	'priority'  => 1,
-	'transport' => 'postMessage',
-	'choices'   => array(
-		'alpha' => true,
-	),
-) );
+wpbf_customizer_field()
+	->id( 'button_text_color' )
+	->type( 'color' )
+	->label( __( 'Font Color', 'page-builder-framework' ) )
+	->priority( 1 )
+	->transport( 'postMessage' )
+	->properties( array(
+		'mode' => 'alpha',
+	) )
+	->addToSection( 'wpbf_button_options' );
 
 // Text color alt.
-Kirki::add_field( 'wpbf', array(
-	'type'      => 'color',
-	'settings'  => 'button_text_color_alt',
-	'label'     => __( 'Hover', 'page-builder-framework' ),
-	'section'   => 'wpbf_button_options',
-	'priority'  => 1,
-	'transport' => 'postMessage',
-	'choices'   => array(
-		'alpha' => true,
-	),
-) );
+wpbf_customizer_field()
+	->id( 'button_text_color_alt' )
+	->type( 'color' )
+	->label( __( 'Hover', 'page-builder-framework' ) )
+	->priority( 1 )
+	->transport( 'postMessage' )
+	->properties( array(
+		'mode' => 'alpha',
+	) )
+	->addToSection( 'wpbf_button_options' );
 
 // Headline.
-new Headline(
-	[
-		'settings' => 'button_primary_headline',
-		'label'    => esc_html__( 'Theme Buttons (Primary)', 'page-builder-framework' ),
-		'tooltip'  => esc_html__( 'Applies to buttons displayed in the themes accent color such as WooCommerce buttons.', 'page-builder-framework' ),
-		'section'  => 'wpbf_button_options',
-		'priority' => 1,
-	]
-);
+wpbf_customizer_field()
+	->id( 'button_primary_headline' )
+	->type( 'headline' )
+	->label( __( 'Theme Buttons (Primary)', 'page-builder-framework' ) )
+	->tooltip( __( 'Applies to buttons displayed in the themes accent color such as WooCommerce buttons.', 'page-builder-framework' ) )
+	->priority( 1 )
+	->addToSection( 'wpbf_button_options' );
 
 // Primary background color.
-Kirki::add_field( 'wpbf', array(
-	'type'      => 'color',
-	'settings'  => 'button_primary_bg_color',
-	'label'     => __( 'Background Color', 'page-builder-framework' ),
-	'section'   => 'wpbf_button_options',
-	'priority'  => 1,
-	'transport' => 'postMessage',
-	'choices'   => array(
-		'alpha' => true,
-	),
-) );
+wpbf_customizer_field()
+	->id( 'button_primary_bg_color' )
+	->type( 'color' )
+	->label( __( 'Background Color', 'page-builder-framework' ) )
+	->priority( 1 )
+	->transport( 'postMessage' )
+	->properties( array(
+		'mode' => 'alpha',
+	) )
+	->addToSection( 'wpbf_button_options' );
 
 // Primary background color alt.
-Kirki::add_field( 'wpbf', array(
-	'type'      => 'color',
-	'settings'  => 'button_primary_bg_color_alt',
-	'label'     => __( 'Hover', 'page-builder-framework' ),
-	'section'   => 'wpbf_button_options',
-	'priority'  => 1,
-	'transport' => 'postMessage',
-	'choices'   => array(
-		'alpha' => true,
-	),
-) );
+wpbf_customizer_field()
+	->id( 'button_primary_bg_color_alt' )
+	->type( 'color' )
+	->label( __( 'Hover', 'page-builder-framework' ) )
+	->priority( 1 )
+	->transport( 'postMessage' )
+	->properties( array(
+		'mode' => 'alpha',
+	) )
+	->addToSection( 'wpbf_button_options' );
 
 // Primary text color.
-Kirki::add_field( 'wpbf', array(
-	'type'      => 'color',
-	'settings'  => 'button_primary_text_color',
-	'label'     => __( 'Primary Font Color', 'page-builder-framework' ),
-	'section'   => 'wpbf_button_options',
-	'priority'  => 1,
-	'transport' => 'postMessage',
-	'choices'   => array(
-		'alpha' => true,
-	),
-) );
+wpbf_customizer_field()
+	->id( 'button_primary_text_color' )
+	->type( 'color' )
+	->label( __( 'Primary Font Color', 'page-builder-framework' ) )
+	->priority( 1 )
+	->transport( 'postMessage' )
+	->properties( array(
+		'mode' => 'alpha',
+	) )
+	->addToSection( 'wpbf_button_options' );
 
 // Primary text color alt.
-Kirki::add_field( 'wpbf', array(
-	'type'      => 'color',
-	'settings'  => 'button_primary_text_color_alt',
-	'label'     => __( 'Hover', 'page-builder-framework' ),
-	'section'   => 'wpbf_button_options',
-	'priority'  => 1,
-	'transport' => 'postMessage',
-	'choices'   => array(
-		'alpha' => true,
-	),
-) );
+wpbf_customizer_field()
+	->id( 'button_primary_text_color_alt' )
+	->type( 'color' )
+	->label( __( 'Hover', 'page-builder-framework' ) )
+	->priority( 1 )
+	->transport( 'postMessage' )
+	->properties( array(
+		'mode' => 'alpha',
+	) )
+	->addToSection( 'wpbf_button_options' );
 
 // Divider.
-new Divider(
-	[
-		'settings' => 'button_border_divider',
-		'section'  => 'wpbf_button_options',
-		'priority' => 1,
-	]
-);
+wpbf_customizer_field()
+	->id( 'button_border_divider' )
+	->type( 'divider' )
+	->priority( 1 )
+	->addToSection( 'wpbf_button_options' );
 
 // Border radius.
-Kirki::add_field( 'wpbf', array(
-	'type'      => 'slider',
-	'settings'  => 'button_border_radius',
-	'label'     => __( 'Border Radius', 'page-builder-framework' ),
-	'section'   => 'wpbf_button_options',
-	'priority'  => 1,
-	'default'   => 0,
-	'transport' => 'postMessage',
-	'choices'   => array(
+wpbf_customizer_field()
+	->id( 'button_border_radius' )
+	->type( 'slider' )
+	->label( __( 'Border Radius', 'page-builder-framework' ) )
+	->defaultValue( 0 )
+	->priority( 1 )
+	->transport( 'postMessage' )
+	->properties( array(
 		'min'  => 0,
 		'max'  => 100,
 		'step' => 1,
-	),
-) );
+	) )
+	->addToSection( 'wpbf_button_options' );
 
 // Border width.
-Kirki::add_field( 'wpbf', array(
-	'type'      => 'slider',
-	'settings'  => 'button_border_width',
-	'label'     => __( 'Border Width', 'page-builder-framework' ),
-	'section'   => 'wpbf_button_options',
-	'priority'  => 1,
-	'default'   => 0,
-	'transport' => 'postMessage',
-	'choices'   => array(
+wpbf_customizer_field()
+	->id( 'button_border_width' )
+	->type( 'slider' )
+	->label( __( 'Border Width', 'page-builder-framework' ) )
+	->defaultValue( 0 )
+	->priority( 1 )
+	->transport( 'postMessage' )
+	->properties( array(
 		'min'  => 0,
 		'max'  => 10,
 		'step' => 1,
-	),
-) );
+	) )
+	->addToSection( 'wpbf_button_options' );
 
 // Divider.
-new Divider(
-	[
-		'settings'        => 'button_border_divider_2',
-		'section'         => 'wpbf_button_options',
-		'priority'        => 1,
-		'active_callback' => [
-			[
-				'setting'  => 'button_border_width',
-				'operator' => '!=',
-				'value'    => 0,
-			],
-		],
-	]
-);
+wpbf_customizer_field()
+	->id( 'button_border_divider_2' )
+	->type( 'divider' )
+	->priority( 1 )
+	->activeCallback( [
+		array(
+			'id'       => 'button_border_width',
+			'operator' => '!=',
+			'value'    => 0,
+		),
+	] )
+	->addToSection( 'wpbf_button_options' );
 
 // Border color.
-Kirki::add_field( 'wpbf', array(
-	'type'            => 'color',
-	'settings'        => 'button_border_color',
-	'label'           => __( 'Border Color', 'page-builder-framework' ),
-	'section'         => 'wpbf_button_options',
-	'priority'        => 1,
-	'transport'       => 'postMessage',
-	'choices'         => array(
-		'alpha' => true,
-	),
-	'active_callback' => array(
+wpbf_customizer_field()
+	->id( 'button_border_color' )
+	->type( 'color' )
+	->label( __( 'Border Color', 'page-builder-framework' ) )
+	->priority( 1 )
+	->transport( 'postMessage' )
+	->properties( array(
+		'mode' => 'alpha',
+	) )
+	->activeCallback( [
 		array(
-			'setting'  => 'button_border_width',
+			'id'       => 'button_border_width',
 			'operator' => '!=',
 			'value'    => 0,
 		),
-	),
-) );
+	] )
+	->addToSection( 'wpbf_button_options' );
 
 // Border color alt.
-Kirki::add_field( 'wpbf', array(
-	'type'            => 'color',
-	'settings'        => 'button_border_color_alt',
-	'label'           => __( 'Hover', 'page-builder-framework' ),
-	'section'         => 'wpbf_button_options',
-	'priority'        => 1,
-	'transport'       => 'postMessage',
-	'choices'         => array(
-		'alpha' => true,
-	),
-	'active_callback' => array(
+wpbf_customizer_field()
+	->id( 'button_border_color_alt' )
+	->type( 'color' )
+	->label( __( 'Hover', 'page-builder-framework' ) )
+	->priority( 1 )
+	->transport( 'postMessage' )
+	->properties( array(
+		'mode' => 'alpha',
+	) )
+	->activeCallback( [
 		array(
-			'setting'  => 'button_border_width',
+			'id'       => 'button_border_width',
 			'operator' => '!=',
 			'value'    => 0,
 		),
-	),
-) );
+	] )
+	->addToSection( 'wpbf_button_options' );
 
 // Primary border color.
-Kirki::add_field( 'wpbf', array(
-	'type'            => 'color',
-	'settings'        => 'button_primary_border_color',
-	'label'           => __( 'Primary Border Color', 'page-builder-framework' ),
-	'section'         => 'wpbf_button_options',
-	'priority'        => 1,
-	'transport'       => 'postMessage',
-	'choices'         => array(
-		'alpha' => true,
-	),
-	'active_callback' => array(
+wpbf_customizer_field()
+	->id( 'button_primary_border_color' )
+	->type( 'color' )
+	->label( __( 'Primary Border Color', 'page-builder-framework' ) )
+	->priority( 1 )
+	->transport( 'postMessage' )
+	->properties( array(
+		'mode' => 'alpha',
+	) )
+	->activeCallback( [
 		array(
-			'setting'  => 'button_border_width',
+			'id'       => 'button_border_width',
 			'operator' => '!=',
 			'value'    => 0,
 		),
-	),
-) );
+	] )
+	->addToSection( 'wpbf_button_options' );
 
 // Primary border color alt.
-Kirki::add_field( 'wpbf', array(
-	'type'            => 'color',
-	'settings'        => 'button_primary_border_color_alt',
-	'label'           => __( 'Hover', 'page-builder-framework' ),
-	'section'         => 'wpbf_button_options',
-	'priority'        => 1,
-	'transport'       => 'postMessage',
-	'choices'         => array(
-		'alpha' => true,
-	),
-	'active_callback' => array(
+wpbf_customizer_field()
+	->id( 'button_primary_border_color_alt' )
+	->type( 'color' )
+	->label( __( 'Hover', 'page-builder-framework' ) )
+	->priority( 1 )
+	->transport( 'postMessage' )
+	->properties( array(
+		'mode' => 'alpha',
+	) )
+	->activeCallback( [
 		array(
-			'setting'  => 'button_border_width',
+			'id'       => 'button_border_width',
 			'operator' => '!=',
 			'value'    => 0,
 		),
-	),
-) );
+	] )
+	->addToSection( 'wpbf_button_options' );
 
 /* Fields - ScrollTop */
 
