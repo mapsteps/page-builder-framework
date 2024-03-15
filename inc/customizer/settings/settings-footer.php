9fa6f4dc8c3e271618af764dd4b110f59219073c
Bagus <25834188+contactjavas@users.noreply.github.com>
2024-03-15 22:57:03 +0700

    convert controls in settings-footer

--- inc/customizer/settings/settings-footer.php
+++ inc/customizer/settings/settings-footer.php
@@ -11,55 +11,52 @@ defined( 'ABSPATH' ) || die( "Can't access directly" );
 /* Panel */
 
 // Footer.
-Kirki::add_panel( 'footer_panel', array(
-	'priority' => 5,
-	'title'    => __( 'Footer', 'page-builder-framework' ),
-) );
+wpbf_customizer_panel()
+	->id( 'footer_panel' )
+	->title( __( 'Footer', 'page-builder-framework' ) )
+	->priority( 5 )
+	->add();
 
 /* Sections - Footer */
 
 // Widget footer.
-Kirki::add_section( 'wpbf_widget_footer_options', array(
-	'title'    => __( 'Widget Area', 'page-builder-framework' ),
-	'panel'    => 'footer_panel',
-	'priority' => 100,
-) );
+wpbf_customizer_section()
+	->id( 'wpbf_widget_footer_options' )
+	->title( __( 'Widget Area', 'page-builder-framework' ) )
+	->priority( 100 )
+	->addToPanel( 'footer_panel' );
 
 // Footer.
-new \Kirki\Section(
-	'wpbf_footer_options',
-	[
-		'title'    => __( 'Footer Bar', 'page-builder-framework' ),
-		'panel'    => 'footer_panel',
-		'priority' => 200,
-		'tabs'     => [
-			'general' => [
-				'label' => esc_html__( 'General', 'page-builder-framework' ),
-			],
-			'design'  => [
-				'label' => esc_html__( 'Design', 'page-builder-framework' ),
-			],
+wpbf_customizer_section()
+	->id( 'wpbf_footer_options' )
+	->title( __( 'Footer Bar', 'page-builder-framework' ) )
+	->priority( 200 )
+	->tabs( [
+		'general' => [
+			'label' => esc_html__( 'General', 'page-builder-framework' ),
 		],
-	]
-);
+		'design'  => [
+			'label' => esc_html__( 'Design', 'page-builder-framework' ),
+		],
+	] )
+	->addToPanel( 'footer_panel' );
 
 /* Fields – Footer */
 
 // Layout.
-Kirki::add_field( 'wpbf', array(
-	'type'            => 'radio-buttonset',
-	'settings'        => 'footer_layout',
-	'label'           => __( 'Footer', 'page-builder-framework' ),
-	'section'         => 'wpbf_footer_options',
-	'default'         => 'two',
-	'priority'        => 1,
-	'tab'             => 'general',
-	'choices'         => array(
+wpbf_customizer_field()
+	->id( 'footer_layout' )
+	->type( 'radio-buttonset' )
+	->tab( 'general' )
+	->label( __( 'Footer', 'page-builder-framework' ) )
+	->defaultValue( 'two' )
+	->priority( 1 )
+	->choices( [
 		'none' => __( 'None', 'page-builder-framework' ),
 		'one'  => __( 'One Column', 'page-builder-framework' ),
 		'two'  => __( 'Two Columns', 'page-builder-framework' ),
-	),
-	'partial_refresh' => array(
+	] )
+	->partialRefresh( [
 		'footerlayout' => array(
 			'container_inclusive' => true,
 			'selector'            => '#footer',
@@ -67,31 +64,35 @@ Kirki::add_field( 'wpbf', array(
 				return get_template_part( 'inc/template-parts/footer' );
 			},
 		),
-	),
-) );
+	] )
+	->addToSection( 'wpbf_footer_options' );
 
 // Column one layout.
-Kirki::add_field( 'wpbf', array(
-	'type'            => 'select',
-	'settings'        => 'footer_column_one_layout',
-	'label'           => __( 'Column 1', 'page-builder-framework' ),
-	'section'         => 'wpbf_footer_options',
-	'default'         => 'text',
-	'priority'        => 2,
-	'tab'             => 'general',
-	'choices'         => array(
+wpbf_customizer_field()
+	->id( 'footer_column_one_layout' )
+	->type( 'select' )
+	->tab( 'general' )
+	->label( __( 'Column 1', 'page-builder-framework' ) )
+	->defaultValue( 'text' )
+	->choices( [
+		'text'    => __( 'Text', 'page-builder-framework' ),
+		'button'  => __( 'Button', 'page-builder-framework' ),
+		'primary' => __( 'Button (Primary)', 'page-builder-framework' ),
+	] )
+	->priority( 2 )
+	->choices( [
 		'none' => __( 'None', 'page-builder-framework' ),
 		'text' => __( 'Text', 'page-builder-framework' ),
 		'menu' => __( 'Menu', 'page-builder-framework' ),
-	),
-	'active_callback' => array(
+	] )
+	->activeCallback( [
 		array(
-			'setting'  => 'footer_layout',
+			'id'       => 'footer_layout',
 			'operator' => '!=',
 			'value'    => 'none',
 		),
-	),
-	'partial_refresh' => array(
+	] )
+	->partialRefresh( [
 		'footercolumnonelayout' => array(
 			'container_inclusive' => true,
 			'selector'            => '#footer',
@@ -99,79 +100,75 @@ Kirki::add_field( 'wpbf', array(
 				return get_template_part( 'inc/template-parts/footer' );
 			},
 		),
-	),
-) );
+	] )
+	->addToSection( 'wpbf_footer_options' );
 
 // Column one.
-Kirki::add_field( 'wpbf', array(
-	'type'            => 'textarea',
-	'settings'        => 'footer_column_one',
-	'label'           => __( 'Text', 'page-builder-framework' ),
-	'section'         => 'wpbf_footer_options',
-	'default'         => __( '&copy; [year] - [blogname] | All rights reserved', 'page-builder-framework' ),
-	'priority'        => 2,
-	'tab'             => 'general',
-	'active_callback' => array(
-		array(
-			'setting'  => 'footer_layout',
-			'operator' => '!=',
-			'value'    => 'none',
-		),
-		array(
-			'setting'  => 'footer_column_one_layout',
-			'operator' => '==',
-			'value'    => 'text',
-		),
-	),
-	'partial_refresh' => array(
+wpbf_customizer_field()
+	->id( 'footer_column_one' )
+	->type( 'textarea' )
+	->tab( 'general' )
+	->label( __( 'Text', 'page-builder-framework' ) )
+	->defaultValue( __( '&copy; [year] - [blogname] | All rights reserved', 'page-builder-framework' ) )
+	->priority( 2 )
+	->partialRefresh( [
 		'footercolumnonecontent' => array(
 			'selector'        => '#footer',
 			'render_callback' => function () {
 				return get_template_part( 'inc/template-parts/footer' );
 			},
 		),
-	),
-) );
+	] )
+	->activeCallback( [
+		array(
+			'id'       => 'footer_layout',
+			'operator' => '!=',
+			'value'    => 'none',
+		),
+		array(
+			'id'       => 'footer_column_one_layout',
+			'operator' => '==',
+			'value'    => 'text',
+		),
+	] )
+	->addToSection( 'wpbf_footer_options' );
 
 // Separator.
-new \Kirki\Pro\Field\Divider(
-	[
-		'settings'        => 'footer_column_two_separator',
-		'section'         => 'wpbf_footer_options',
-		'priority'        => 3,
-		'tab'             => 'general',
-		'active_callback' => [
-			[
-				'setting'  => 'footer_layout',
-				'operator' => '==',
-				'value'    => 'two',
-			],
-		],
-	]
-);
-
-// Column two layout.
-Kirki::add_field( 'wpbf', array(
-	'type'            => 'select',
-	'settings'        => 'footer_column_two_layout',
-	'label'           => __( 'Column 2', 'page-builder-framework' ),
-	'section'         => 'wpbf_footer_options',
-	'default'         => 'text',
-	'priority'        => 3,
-	'tab'             => 'general',
-	'choices'         => array(
-		'none' => __( 'None', 'page-builder-framework' ),
-		'text' => __( 'Text', 'page-builder-framework' ),
-		'menu' => __( 'Menu', 'page-builder-framework' ),
-	),
-	'active_callback' => array(
+wpbf_customizer_field()
+	->id( 'footer_column_two_separator' )
+	->type( 'divider' )
+	->tab( 'general' )
+	->priority( 3 )
+	->activeCallback( [
 		array(
-			'setting'  => 'footer_layout',
+			'id'       => 'footer_layout',
 			'operator' => '==',
 			'value'    => 'two',
 		),
-	),
-	'partial_refresh' => array(
+	] )
+	->addToSection( 'wpbf_footer_options' );
+
+// Column two layout.
+wpbf_customizer_field()
+	->id( 'footer_column_two_layout' )
+	->type( 'select' )
+	->tab( 'general' )
+	->label( __( 'Column 2', 'page-builder-framework' ) )
+	->defaultValue( 'text' )
+	->choices( [
+		'none' => __( 'None', 'page-builder-framework' ),
+		'text' => __( 'Text', 'page-builder-framework' ),
+		'menu' => __( 'Menu', 'page-builder-framework' ),
+	] )
+	->priority( 3 )
+	->activeCallback( [
+		array(
+			'id'       => 'footer_layout',
+			'operator' => '==',
+			'value'    => 'two',
+		),
+	] )
+	->partialRefresh( [
 		'footercolumntwolayout' => array(
 			'container_inclusive' => true,
 			'selector'            => '#footer',
@@ -179,191 +176,183 @@ Kirki::add_field( 'wpbf', array(
 				return get_template_part( 'inc/template-parts/footer' );
 			},
 		),
-	),
-) );
+	] )
+	->addToSection( 'wpbf_footer_options' );
 
 // Column two.
-Kirki::add_field( 'wpbf', array(
-	'type'            => 'textarea',
-	'settings'        => 'footer_column_two',
-	'label'           => __( 'Text', 'page-builder-framework' ),
-	'section'         => 'wpbf_footer_options',
-	'default'         => __( 'Powered by [theme_author]', 'page-builder-framework' ),
-	'priority'        => 3,
-	'tab'             => 'general',
-	'active_callback' => array(
-		array(
-			'setting'  => 'footer_layout',
-			'operator' => '==',
-			'value'    => 'two',
-		),
-		array(
-			'setting'  => 'footer_column_two_layout',
-			'operator' => '==',
-			'value'    => 'text',
-		),
-	),
-	'partial_refresh' => array(
+wpbf_customizer_field()
+	->id( 'footer_column_two' )
+	->type( 'textarea' )
+	->tab( 'general' )
+	->label( __( 'Text', 'page-builder-framework' ) )
+	->defaultValue( __( 'Powered by [theme_author]', 'page-builder-framework' ) )
+	->priority( 3 )
+	->partialRefresh( [
 		'footercolumntwocontent' => array(
 			'selector'        => '#footer',
 			'render_callback' => function () {
 				return get_template_part( 'inc/template-parts/footer' );
 			},
 		),
-	),
-) );
+	] )
+	->activeCallback( [
+		array(
+			'id'       => 'footer_layout',
+			'operator' => '==',
+			'value'    => 'two',
+		),
+		array(
+			'id'       => 'footer_column_two_layout',
+			'operator' => '==',
+			'value'    => 'text',
+		),
+	] )
+	->addToSection( 'wpbf_footer_options' );
 
 // Width.
-Kirki::add_field( 'wpbf', array(
-	'type'            => 'dimension',
-	'label'           => __( 'Footer Width', 'page-builder-framework' ),
-	'description'     => __( 'Default: 1200px', 'page-builder-framework' ),
-	'settings'        => 'footer_width',
-	'section'         => 'wpbf_footer_options',
-	'priority'        => 5,
-	'transport'       => 'postMessage',
-	'tab'             => 'general',
-	'active_callback' => array(
+wpbf_customizer_field()
+	->id( 'footer_width' )
+	->type( 'dimension' )
+	->tab( 'general' )
+	->label( __( 'Footer Width', 'page-builder-framework' ) )
+	->description( __( 'Default: 1200px', 'page-builder-framework' ) )
+	->priority( 5 )
+	->transport( 'postMessage' )
+	->activeCallback( [
 		array(
-			'setting'  => 'footer_layout',
+			'id'       => 'footer_layout',
 			'operator' => '!=',
 			'value'    => 'none',
 		),
-	),
-) );
+	] )
+	->addToSection( 'wpbf_footer_options' );
 
 // Footer height.
-Kirki::add_field( 'wpbf', array(
-	'type'            => 'slider',
-	'settings'        => 'footer_height',
-	'label'           => __( 'Height', 'page-builder-framework' ),
-	'section'         => 'wpbf_footer_options',
-	'priority'        => 6,
-	'default'         => 20,
-	'transport'       => 'postMessage',
-	'tab'             => 'general',
-	'choices'         => array(
+wpbf_customizer_field()
+	->id( 'footer_height' )
+	->type( 'slider' )
+	->tab( 'general' )
+	->label( __( 'Height', 'page-builder-framework' ) )
+	->priority( 6 )
+	->defaultValue( 20 )
+	->transport( 'postMessage' )
+	->properties( array(
 		'min'  => 1,
 		'max'  => 100,
 		'step' => 1,
-	),
-	'active_callback' => array(
+	) )
+	->activeCallback( [
 		array(
-			'setting'  => 'footer_layout',
+			'id'       => 'footer_layout',
 			'operator' => '!=',
 			'value'    => 'none',
 		),
-	),
-) );
+	] )
+	->addToSection( 'wpbf_footer_options' );
 
 // Background color.
-Kirki::add_field( 'wpbf', array(
-	'type'            => 'color',
-	'settings'        => 'footer_bg_color',
-	'label'           => __( 'Background Color', 'page-builder-framework' ),
-	'section'         => 'wpbf_footer_options',
-	'default'         => '#f5f5f7',
-	'transport'       => 'postMessage',
-	'priority'        => 7,
-	'tab'             => 'design',
-	'choices'         => array(
-		'alpha' => true,
-	),
-	'active_callback' => array(
+wpbf_customizer_field()
+	->id( 'footer_bg_color' )
+	->type( 'color' )
+	->tab( 'design' )
+	->label( __( 'Background Color', 'page-builder-framework' ) )
+	->defaultValue( '#f5f5f7' )
+	->priority( 7 )
+	->transport( 'postMessage' )
+	->properties( array(
+		'mode' => 'alpha',
+	) )
+	->activeCallback( [
 		array(
-			'setting'  => 'footer_layout',
+			'id'       => 'footer_layout',
 			'operator' => '!=',
 			'value'    => 'none',
 		),
-	),
-) );
+	] )
+	->addToSection( 'wpbf_footer_options' );
 
 // Font color.
-Kirki::add_field( 'wpbf', array(
-	'type'            => 'color',
-	'settings'        => 'footer_font_color',
-	'label'           => __( 'Font Color', 'page-builder-framework' ),
-	'section'         => 'wpbf_footer_options',
-	'transport'       => 'postMessage',
-	'priority'        => 8,
-	'tab'             => 'design',
-	'choices'         => array(
-		'alpha' => true,
-	),
-	'active_callback' => array(
+wpbf_customizer_field()
+	->id( 'footer_font_color' )
+	->type( 'color' )
+	->tab( 'design' )
+	->label( __( 'Font Color', 'page-builder-framework' ) )
+	->priority( 8 )
+	->transport( 'postMessage' )
+	->properties( array(
+		'mode' => 'alpha',
+	) )
+	->activeCallback( [
 		array(
-			'setting'  => 'footer_layout',
+			'id'       => 'footer_layout',
 			'operator' => '!=',
 			'value'    => 'none',
 		),
-	),
-) );
+	] )
+	->addToSection( 'wpbf_footer_options' );
 
 // Accent color.
-Kirki::add_field( 'wpbf', array(
-	'type'            => 'color',
-	'settings'        => 'footer_accent_color',
-	'label'           => __( 'Accent Color', 'page-builder-framework' ),
-	'section'         => 'wpbf_footer_options',
-	'priority'        => 9,
-	'transport'       => 'postMessage',
-	'tab'             => 'design',
-	'choices'         => array(
-		'alpha' => true,
-	),
-	'active_callback' => array(
+wpbf_customizer_field()
+	->id( 'footer_accent_color' )
+	->type( 'color' )
+	->tab( 'design' )
+	->label( __( 'Accent Color', 'page-builder-framework' ) )
+	->priority( 9 )
+	->transport( 'postMessage' )
+	->properties( array(
+		'mode' => 'alpha',
+	) )
+	->activeCallback( [
 		array(
-			'setting'  => 'footer_layout',
+			'id'       => 'footer_layout',
 			'operator' => '!=',
 			'value'    => 'none',
 		),
-	),
-) );
+	] )
+	->addToSection( 'wpbf_footer_options' );
 
 // Accent color alt.
-Kirki::add_field( 'wpbf', array(
-	'type'            => 'color',
-	'settings'        => 'footer_accent_color_alt',
-	'label'           => __( 'Hover', 'page-builder-framework' ),
-	'section'         => 'wpbf_footer_options',
-	'priority'        => 10,
-	'transport'       => 'postMessage',
-	'tab'             => 'design',
-	'choices'         => array(
-		'alpha' => true,
-	),
-	'active_callback' => array(
+wpbf_customizer_field()
+	->id( 'footer_accent_color_alt' )
+	->type( 'color' )
+	->tab( 'design' )
+	->label( __( 'Hover', 'page-builder-framework' ) )
+	->priority( 10 )
+	->transport( 'postMessage' )
+	->properties( array(
+		'mode' => 'alpha',
+	) )
+	->activeCallback( [
 		array(
-			'setting'  => 'footer_layout',
+			'id'       => 'footer_layout',
 			'operator' => '!=',
 			'value'    => 'none',
 		),
-	),
-) );
+	] )
+	->addToSection( 'wpbf_footer_options' );
 
 // Font size.
-Kirki::add_field( 'wpbf', array(
-	'type'            => 'input_slider',
-	'label'           => __( 'Font Size', 'page-builder-framework' ),
-	'settings'        => 'footer_font_size',
-	'section'         => 'wpbf_footer_options',
-	'priority'        => 11,
-	'default'         => '14px',
-	'transport'       => 'postMessage',
-	'tab'             => 'design',
-	'active_callback' => array(
+wpbf_customizer_field()
+	->id( 'footer_font_size' )
+	->type( 'input-slider' )
+	->tab( 'design' )
+	->label( __( 'Font Size', 'page-builder-framework' ) )
+	->priority( 11 )
+	->defaultValue( '14px' )
+	->transport( 'postMessage' )
+	->activeCallback( [
 		array(
-			'setting'  => 'footer_layout',
+			'id'       => 'footer_layout',
 			'operator' => '!=',
 			'value'    => 'none',
 		),
-	),
-	'choices'         => array(
+	] )
+	->properties( [
 		'min'  => 0,
 		'max'  => 50,
 		'step' => 1,
-	),
-) );
+	] )
+	->addToSection( 'wpbf_footer_options' );
 
 if ( ! wpbf_is_premium() ) {
 
@@ -374,13 +363,12 @@ if ( ! wpbf_is_premium() ) {
 		__( 'Learn More', 'page-builder-framework' )
 	);
 
-	Kirki::add_field( 'wpbf', array(
-		'type'     => 'custom',
-		'settings' => 'wpbf_premium_ad_widget_footer',
-		'section'  => 'wpbf_widget_footer_options',
-		'default'  => $wpbf_premium_ad_link,
-		'priority' => 9999,
-	) );
+	wpbf_customizer_field()
+		->id( 'wpbf_premium_ad_widget_footer' )
+		->type( 'custom' )
+		->defaultValue( $wpbf_premium_ad_link )
+		->priority( 9999 )
+		->addToSection( 'wpbf_widget_footer_options' );
 
 	// Premium notice.
 	$wpbf_premium_ad_link = sprintf(
@@ -389,12 +377,11 @@ if ( ! wpbf_is_premium() ) {
 		__( 'Learn More', 'page-builder-framework' )
 	);
 
-	Kirki::add_field( 'wpbf', array(
-		'type'     => 'custom',
-		'settings' => 'wpbf_premium_ad_footer',
-		'section'  => 'wpbf_footer_options',
-		'default'  => '<hr style="border-top: 1px solid #ccc; border-bottom: 1px solid #f8f8f8">' . $wpbf_premium_ad_link,
-		'priority' => 9999,
-	) );
+	wpbf_customizer_field()
+		->id( 'wpbf_premium_ad_footer' )
+		->type( 'custom' )
+		->defaultValue( '<hr style="border-top: 1px solid #ccc; border-bottom: 1px solid #f8f8f8">' . $wpbf_premium_ad_link )
+		->priority( 9999 )
+		->addToSection( 'wpbf_footer_options' );
 
 }
