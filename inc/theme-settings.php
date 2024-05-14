2fe5e9ddba161da0cebbe2b47dcd17006fd97baf
Bagus <25834188+contactjavas@users.noreply.github.com>
2024-05-15 02:33:11 +0700

    correcting the min version checking

--- inc/theme-settings.php
+++ inc/theme-settings.php
@@ -167,8 +167,8 @@ function wpbf_show_compatibility_notice() {
 		return;
 	}
 
-	// Stop here if Premium Add-On is not below or equal the minimum required version.
-	if ( ! version_compare( WPBF_PREMIUM_VERSION, WPBF_PREMIUM_MIN_VERSION, '<=' ) ) {
+	// Stop here if Premium Add-On is not below the minimum required version.
+	if ( ! version_compare( WPBF_PREMIUM_VERSION, WPBF_PREMIUM_MIN_VERSION, '<' ) ) {
 		return;
 	}
 
