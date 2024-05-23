0254516dc6545081759b1af62bc101c75663782a
Bagus <25834188+contactjavas@users.noreply.github.com>
2024-05-23 23:18:02 +0700

    fatale error fix: check for kirki plugin before checking their classes

--- inc/customizer/customizer-functions.php
+++ inc/customizer/customizer-functions.php
@@ -89,14 +89,16 @@ function wpbf_customizer_field() {
 }
 
 // Kirki compatiblity.
-require_once WPBF_THEME_DIR . '/Customizer/Compatibility/kirki-compatibility.php';
-require_once WPBF_THEME_DIR . '/Customizer/Compatibility/kirki-panel-section-compatibility.php';
-require_once WPBF_THEME_DIR . '/Customizer/Compatibility/kirki-control-base-compatibility.php';
-require_once WPBF_THEME_DIR . '/Customizer/Compatibility/kirki-fields-compatibility.php';
-require_once WPBF_THEME_DIR . '/Customizer/Compatibility/kirki-unsupported-fields-compatibility.php';
-require_once WPBF_THEME_DIR . '/Customizer/Compatibility/kirki-pro-fields-compatibility.php';
-require_once WPBF_THEME_DIR . '/Customizer/Compatibility/kirki-class-aliases.php';
-require_once WPBF_THEME_DIR . '/Customizer/Compatibility/wpbf-old-controls-compatibility.php';
+if ( ! is_plugin_active( 'kirki/kirki.php' ) ) {
+	require_once WPBF_THEME_DIR . '/Customizer/Compatibility/kirki-compatibility.php';
+	require_once WPBF_THEME_DIR . '/Customizer/Compatibility/kirki-panel-section-compatibility.php';
+	require_once WPBF_THEME_DIR . '/Customizer/Compatibility/kirki-control-base-compatibility.php';
+	require_once WPBF_THEME_DIR . '/Customizer/Compatibility/kirki-fields-compatibility.php';
+	require_once WPBF_THEME_DIR . '/Customizer/Compatibility/kirki-unsupported-fields-compatibility.php';
+	require_once WPBF_THEME_DIR . '/Customizer/Compatibility/kirki-pro-fields-compatibility.php';
+	require_once WPBF_THEME_DIR . '/Customizer/Compatibility/kirki-class-aliases.php';
+	require_once WPBF_THEME_DIR . '/Customizer/Compatibility/wpbf-old-controls-compatibility.php';
+}
 
 if ( ! function_exists( 'wpbf_get_theme_mod_value' ) ) {
 
