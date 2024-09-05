89c546f530da0a4c4e20dcfa777ed9ac4a26abfd
Bagus <25834188+contactjavas@users.noreply.github.com>
2024-09-05 11:16:34 +0700

    header builder search section

--- inc/customizer/settings/header-builder/search-section.php
+++ inc/customizer/settings/header-builder/search-section.php
@@ -8,26 +8,59 @@
 
 defined( 'ABSPATH' ) || die( "Can't access directly" );
 
+$section_id = 'wpbf_header_builder_search_section';
+
 wpbf_customizer_section()
-	->id( 'wpbf_header_builder_search_section' )
+	->id( $section_id )
 	->type( 'invisible' )
 	->title( __( 'Search', 'page-builder-framework' ) )
 	->priority( 3 )
 	->addToPanel( 'header_panel' );
 
+$control_id_prefix = 'wpbf_header_builder_search_';
+
+$partial_refresh_key_prefix = 'headerbuilder_search_';
+
+$partial_refresh_args = array(
+	'container_inclusive' => true,
+	'selector'            => '#header',
+	'render_callback'     => function () {
+		return get_template_part( 'inc/template-parts/header' );
+	},
+);
+
 wpbf_customizer_field()
-	->id( 'wpbf_header_builder_search_icon' )
-	->setting( 'menu_search_icon' )
-	->type( 'toggle' )
-	->tab( 'general' )
-	->label( __( 'Search Icon', 'page-builder-framework' ) )
-	->partialRefresh( [
-		'menusearchicon' => array(
-			'container_inclusive' => true,
-			'selector'            => '#header',
-			'render_callback'     => function () {
-				return get_template_part( 'inc/template-parts/header' );
-			},
-		),
+	->id( $control_id_prefix . 'icon_size' )
+	->type( 'responsive-input-slider' )
+	->label( __( 'Icon Size', 'page-builder-framework' ) )
+	->defaultValue( 16 )
+	->transport( 'postMessage' )
+	->properties( [
+		'min'  => 0,
+		'max'  => 100,
+		'step' => 1,
 	] )
-	->addToSection( 'wpbf_header_builder_search_section' );
+	->partialRefresh( [
+		$partial_refresh_key_prefix . 'icon_size' => $partial_refresh_args,
+	] )
+	->addToSection( $section_id );
+
+wpbf_customizer_field()
+	->id( $control_id_prefix . 'icon_color' )
+	->type( 'color' )
+	->label( __( 'Icon Color', 'page-builder-framework' ) )
+	->transport( 'postMessage' )
+	->properties( [
+		'mode' => 'alpha',
+	] )
+	->addToSection( $section_id );
+
+wpbf_customizer_field()
+	->id( $control_id_prefix . 'icon_accent_color' )
+	->type( 'color' )
+	->label( __( 'Icon Accent Color', 'page-builder-framework' ) )
+	->transport( 'postMessage' )
+	->properties( [
+		'mode' => 'alpha',
+	] )
+	->addToSection( $section_id );
