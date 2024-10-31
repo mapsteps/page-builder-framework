88f79f1c0c0133aed42c52182e89adfd16e1355a
Bagus <25834188+contactjavas@users.noreply.github.com>
2024-10-31 15:12:11 +0700

    bottom row: implement it similar to the main (middle) row

--- inc/customizer/settings/header-builder/bottom-row-section.php
+++ inc/customizer/settings/header-builder/bottom-row-section.php
@@ -63,12 +63,11 @@ wpbf_customizer_field()
 	->addToSection( $section_id );
 
 wpbf_customizer_field()
-	->id( $control_id_prefix . 'use_container' )
-	->type( 'toggle' )
+	->id( $control_id_prefix . 'max_width' )
+	->type( 'dimension' )
 	->tab( 'general' )
-	->label( __( 'Use Container', 'page-builder-framework' ) )
-	->description( __( "Whether to make this row's layout contained (boxed).", 'page-builder-framework' ) )
-	->defaultValue( true )
+	->label( __( 'Width', 'page-builder-framework' ) )
+	->description( __( 'Default: 1200px', 'page-builder-framework' ) )
 	->transport( 'postMessage' )
 	->addToSection( $section_id );
 
@@ -79,13 +78,10 @@ wpbf_customizer_field()
 	->type( 'responsive-input-slider' )
 	->tab( 'design' )
 	->label( __( 'Min Height', 'page-builder-framework' ) )
-	->defaultValue( [
-		'desktop' => '50px',
-	] )
 	->transport( 'postMessage' )
 	->properties( [
 		'min'  => 0,
-		'max'  => 600,
+		'max'  => 1000,
 		'step' => 1,
 	] )
 	->addToSection( $section_id );
@@ -102,6 +98,20 @@ wpbf_customizer_field()
 	->properties( [
 		'min'  => 0,
 		'max'  => 200,
+		'step' => 1,
+	] )
+	->addToSection( $section_id );
+
+wpbf_customizer_field()
+	->id( $control_id_prefix . 'font_size' )
+	->type( 'responsive-input-slider' )
+	->tab( 'design' )
+	->label( __( 'Font Size', 'page-builder-framework' ) )
+	->description( __( 'Default: 16px', 'page-builder-framework' ) )
+	->transport( 'postMessage' )
+	->properties( [
+		'min'  => 1,
+		'max'  => 100,
 		'step' => 1,
 	] )
 	->addToSection( $section_id );
@@ -132,4 +142,19 @@ wpbf_customizer_field()
 	->properties( [
 		'mode' => 'alpha',
 	] )
+	->addToSection( $section_id );
+
+wpbf_customizer_field()
+	->id( $control_id_prefix . 'accent_colors' )
+	->type( 'multicolor' )
+	->tab( 'design' )
+	->label( __( 'Accent Color', 'page-builder-framework' ) )
+	->transport( 'postMessage' )
+	->choices( array(
+		'default' => __( 'Default', 'page-builder-framework' ),
+		'hover'   => __( 'Hover', 'page-builder-framework' ),
+	) )
+	->properties( array(
+		'mode' => 'alpha',
+	) )
 	->addToSection( $section_id );
