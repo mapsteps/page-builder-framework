be339412025e2bdbee0f551e3c7710811560e3f7
Bagus <25834188+contactjavas@users.noreply.github.com>
2025-01-13 19:56:59 +0700

    build system refinement

--- wpbf.mjs
+++ wpbf.mjs
@@ -1,6 +1,105 @@
 #!/usr/bin/env zx
 
 import "zx/globals";
+import {
+	intro,
+	outro,
+	isCancel,
+	cancel,
+	text,
+	select,
+	spinner,
+} from "@clack/prompts";
+import { Parcel } from "@parcel/core";
+
+const loadingSpinner = spinner();
+
+intro(`Page Builder Framework`);
+
+const selectedTask = await select({
+	message: "Please select a task.",
+	options: [
+		{ value: "build-wp-plugin", label: "Build WP plugin" },
+		{ value: "build-customizer-control", label: "Build customizer control" },
+		{ value: "build-asset", label: "Build asset" },
+	],
+});
+
+if (isCancel(selectedTask)) {
+	cancel("Task cancelled.");
+	process.exit(0);
+}
+
+if (selectedTask === "build-wp-plugin") {
+	loadingSpinner.start("Building WP plugin...");
+	buildWpPlugin();
+	loadingSpinner.stop();
+} else {
+	if (selectedTask === "build-customizer-control") {
+		const controlName = await text({
+			message: "Insert the customizer control name (e.g: checkbox):",
+		});
+
+		if (isCancel(controlName)) {
+			cancel("Build customizer control cancelled.");
+			process.exit(0);
+		}
+
+		loadingSpinner.start(`Building customizer control: ${controlName}...`);
+		//
+		loadingSpinner.stop();
+	}
+}
+
+outro(`Done!`);
+
+/**
+ * Bundle the customizer control.
+ *
+ * @param {string} controlName The customizer control name. E.g: checkbox.
+ */
+async function bundleCustomizerControl(controlName) {
+	const pascalCaseControlName = controlName.replace(/-([a-z])/g, (_, letter) =>
+		letter.toUpperCase(),
+	);
+
+	const controlPath = path.join(
+		__dirname,
+		`Customizer/Controls/${pascalCaseControlName}/src/${controlName}-control.ts`,
+	);
+
+	const bundler = new Parcel({
+		entries: controlPath,
+		defaultConfig: "@parcel/config-default",
+		mode: "production",
+		defaultTargetOptions: {
+			engines: {
+				browsers: ["> 0.5%", "last 2 versions", "not dead"],
+			},
+		},
+		targets: {
+			main: {
+				distDir: path.join(__dirname, "Customizer/Controls/Checkbox/dist"),
+				engines: {
+					browsers: ["> 0.5%", "last 2 versions", "not dead"],
+				},
+				outputFormat: "esmodule",
+				sourceMap: true,
+				optimize: true,
+			},
+		},
+	});
+
+	try {
+		let { bundleGraph, buildTime } = await bundler.run();
+		let bundles = bundleGraph.getBundles();
+		console.log(`✨ Built ${bundles.length} bundles in ${buildTime}ms!`);
+	} catch (err) {
+		console.log(
+			typeof err === "object" && err && "diagnostics" in err ? err.diagnostics : err,
+		);
+	}
+}
 
 const themeBuildDir = "./build";
 
@@ -114,5 +213,3 @@ function buildWpPlugin() {
 	copyFilesAndDir(".", wpbfBuildDir);
 	deleteMapFiles(wpbfBuildDir);
 }
-
-buildWpPlugin();
