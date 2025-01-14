be3533a59802b152f808c3836f5557f2d051ad0a
Bagus <25834188+contactjavas@users.noreply.github.com>
2025-01-14 15:50:27 +0700

    run `pnpm wpbf` to run dev tasks

--- wpbf.mjs
+++ wpbf.mjs
@@ -1,6 +1,7 @@
 #!/usr/bin/env zx
 
 import "zx/globals";
+import { fileURLToPath } from "url";
 import {
 	intro,
 	outro,
@@ -11,143 +12,12 @@ import {
 	spinner,
 } from "@clack/prompts";
 import { Parcel } from "@parcel/core";
+import { dirname } from "path";
 
-const loadingSpinner = spinner();
-
-// Parcel shared configuration
-const parcelConfig = {
-	defaultConfig: "@parcel/config-default",
-	mode: "production",
-	defaultTargetOptions: {
-		engines: {
-			browsers: ["> 0.5%", "last 2 versions", "not dead"],
-		},
-	},
-	namers: ["parcel-namer-rewrite"],
-	resolvers: ["parcel-resolver-ignore", "..."],
-	additionalResolveOptions: {
-		alias: {
-			// Your alias configuration from package.json
-			react: { global: "React" },
-			"react-dom": { global: "ReactDOM" },
-			"react/jsx-runtime": { global: "_jsx" },
-			jquery: { global: "jQuery" },
-			wp: { global: "wp" },
-			lodash: { global: "_" },
-			"@wordpress/editor": { global: "wp.editor" },
-			"@wordpress/i18n": { global: "wp.i18n" },
-			"@wordpress/hooks": { global: "wp.hooks" },
-		},
-	},
-	nameConfig: {
-		rules: {
-			"(.*).js": "$1-min.js",
-			"(.*).css": "$1-min.css",
-		},
-		hashing: "never",
-		silent: true,
-	},
-	ignore: ["img/.+"],
-};
-
-intro(`Page Builder Framework`);
-
-const selectedTask = await select({
-	message: "Please select a task.",
-	options: [
-		{ value: "build-wp-plugin", label: "Build WP plugin" },
-		{ value: "build-customizer-control", label: "Build customizer control" },
-		{ value: "build-asset", label: "Build asset" },
-	],
-});
-
-if (isCancel(selectedTask)) {
-	cancel("Task cancelled.");
-	process.exit(0);
-}
-
-if (selectedTask === "build-wp-plugin") {
-	loadingSpinner.start("Building WP plugin...");
-	buildWpPlugin();
-	loadingSpinner.stop();
-} else {
-	if (selectedTask === "build-customizer-control") {
-		const controlName = await text({
-			message: "Insert the customizer control name (e.g: checkbox):",
-		});
-
-		if (isCancel(controlName)) {
-			cancel("Build customizer control cancelled.");
-			process.exit(0);
-		}
-
-		loadingSpinner.start(`Building customizer control: ${controlName}...`);
-		bundleCustomizerControl(controlName);
-		loadingSpinner.stop();
-	}
-}
-
-outro(`Done!`);
-
-/**
- * Bundle the customizer control.
- *
- * @param {string} controlName The customizer control name. E.g: checkbox.
- */
-async function bundleCustomizerControl(controlName) {
-	const pascalCaseControlName = controlName.replace(/-([a-z])/g, (_, letter) =>
-		letter.toUpperCase(),
-	);
-
-	const controlPath = path.join(
-		__dirname,
-		`Customizer/Controls/${pascalCaseControlName}/src/${controlName}-control.ts`,
-	);
-
-	const bundler = new Parcel({
-		...parcelConfig,
-		entries: controlPath,
-		targets: {
-			main: {
-				distDir: path.join(
-					__dirname,
-					`Customizer/Controls/${pascalCaseControlName}/dist`,
-				),
-				engines: {
-					browsers: ["> 0.5%", "last 2 versions", "not dead"],
-				},
-				outputFormat: "esmodule",
-				sourceMap: true,
-				optimize: true,
-			},
-		},
-	});
-
-	try {
-		let { bundleGraph, buildTime } = await bundler.run();
-		let bundles = bundleGraph.getBundles();
-		console.log(`✨ Built ${bundles.length} bundles in ${buildTime}ms!`);
-	} catch (err) {
-		console.log(
-			typeof err === "object" && err && "diagnostics" in err
-				? err.diagnostics
-				: err,
-		);
-	}
-}
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const themeBuildDir = "./build";
-
-/**
- * Delete the build directory if it exists.
- *
- * @param {string} dir The build directory path.
- */
-function deleteIfDirExists(dir) {
-	if (fs.existsSync(dir)) {
-		fs.rmSync(dir, { recursive: true });
-	}
-}
 
 const rootFilesAndDirsToSkip = [
 	".parcel-cache",
@@ -172,6 +42,166 @@ const rootFilesAndDirsToSkip = [
 	"types.js",
 	"wpbf.mjs",
 ];
+
+const loadingSpinner = spinner();
+
+// Parcel shared configuration
+const parcelConfig = {
+	defaultConfig: "@parcel/config-default",
+	mode: "production",
+	defaultTargetOptions: {
+		engines: {
+			browsers: ["> 0.5%", "last 2 versions", "not dead"],
+		},
+	},
+	additionalResolveOptions: {
+		// Handled in package.json
+		/*
+		alias: {
+			react: { global: "React" },
+			"react-dom": { global: "ReactDOM" },
+			"react/jsx-runtime": { global: "_jsx" },
+			jquery: { global: "jQuery" },
+			wp: { global: "wp" },
+			lodash: { global: "_" },
+			"@wordpress/editor": { global: "wp.editor" },
+			"@wordpress/i18n": { global: "wp.i18n" },
+			"@wordpress/hooks": { global: "wp.hooks" },
+		},
+		*/
+	},
+	// Handled in package.json
+	/*
+	namers: ["parcel-namer-rewrite"],
+	resolvers: ["parcel-resolver-ignore", "..."],
+	nameConfig: {
+		rules: {
+			"(.*).js": "$1-min.js",
+			"(.*).css": "$1-min.css",
+		},
+		hashing: "never",
+		silent: true,
+	},
+	ignore: ["img/.+"],
+	*/
+};
+
+async function main() {
+	intro(`Page Builder Framework`);
+
+	const selectedTask = await select({
+		message: "Please select a task.",
+		options: [
+			{ value: "build-wp-plugin", label: "Build WP plugin" },
+			{ value: "build-customizer-control", label: "Build customizer control" },
+			{ value: "build-asset", label: "Build asset" },
+		],
+	});
+
+	if (isCancel(selectedTask)) {
+		cancel("Task cancelled.");
+		process.exit(0);
+	}
+
+	if (selectedTask === "build-wp-plugin") {
+		loadingSpinner.start("Building WP plugin...");
+		buildWpPlugin();
+		loadingSpinner.stop();
+	} else {
+		if (selectedTask === "build-customizer-control") {
+			const controlName = await text({
+				message: "Insert the customizer control name (e.g: checkbox):",
+			});
+
+			if (isCancel(controlName)) {
+				cancel("Build customizer control cancelled.");
+				process.exit(0);
+			}
+
+			loadingSpinner.start(`Building customizer control: ${controlName}...`);
+
+			deleteIfDirExists(
+				path.join(
+					__dirname,
+					`Customizer/Controls/${toPascalCase(controlName)}/dist`,
+				),
+			);
+
+			const response = await bundleCustomizerControl(controlName);
+			loadingSpinner.stop(response);
+		}
+	}
+
+	outro(`Done!`);
+}
+
+/**
+ * Bundle the customizer control.
+ *
+ * @param {string} controlName The customizer control name. E.g: checkbox.
+ */
+async function bundleCustomizerControl(controlName) {
+	const pascalCaseControlName = toPascalCase(controlName);
+
+	const controlPath = path.join(
+		__dirname,
+		`Customizer/Controls/${pascalCaseControlName}/src/${controlName}-control.ts`,
+	);
+
+	const bundler = new Parcel({
+		...parcelConfig,
+		entries: controlPath,
+		shouldDisableCache: true,
+		targets: {
+			main: {
+				distDir: path.join(
+					__dirname,
+					`Customizer/Controls/${pascalCaseControlName}/dist`,
+				),
+				engines: {
+					browsers: ["> 0.5%", "last 2 versions", "not dead"],
+				},
+				// outputFormat: "esmodule",
+				sourceMap: true,
+				optimize: true,
+			},
+		},
+	});
+
+	try {
+		let { bundleGraph, buildTime } = await bundler.run();
+		let bundles = bundleGraph.getBundles();
+
+		return `✨ Built ${bundles.length} bundles in ${buildTime}ms!`;
+	} catch (err) {
+		return String(
+			typeof err === "object" && err && "diagnostics" in err
+				? err.diagnostics
+				: err,
+		);
+	}
+}
+
+/**
+ * Convert a string to PascalCase.
+ *
+ * @param {string} str The string to convert.
+ * @returns {string} The converted string.
+ */
+function toPascalCase(str) {
+	return str.replace(/-([a-z])/g, (_, letter) => letter.toUpperCase());
+}
+
+/**
+ * Delete the build directory if it exists.
+ *
+ * @param {string} dir The build directory path.
+ */
+function deleteIfDirExists(dir) {
+	if (fs.existsSync(dir)) {
+		fs.rmSync(dir, { recursive: true });
+	}
+}
 
 /**
  * Copy the files and directories from the source directory to the destination directory.
@@ -248,3 +278,5 @@ function buildWpPlugin() {
 	copyFilesAndDir(".", wpbfBuildDir);
 	deleteMapFiles(wpbfBuildDir);
 }
+
+main();
