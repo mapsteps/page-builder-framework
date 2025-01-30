17115b066161bedf5053fda88e7a026447f5ae2a
Bagus <25834188+contactjavas@users.noreply.github.com>
2025-01-31 02:51:13 +0700

    hamburger style settings

--- inc/customizer/settings/header-builder/mobile/menu-trigger-section.php
+++ inc/customizer/settings/header-builder/mobile/menu-trigger-section.php
@@ -37,7 +37,7 @@ wpbf_customizer_field()
 	->defaultValue( 'none' )
 	->priority( 1 )
 	->choices( [
-		''          => '
+		'none'      => '
 		<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
 			<circle cx="50" cy="50" r="35" 
 					fill="none" 
@@ -51,9 +51,27 @@ wpbf_customizer_field()
 				stroke-linecap="round"/>
 		</svg>
 		',
-		'variant-1' => '<svg width="100%" viewBox="0 0 100 70"><path d="M61.2,36.2H38.8c-0.7,0-1.2-0.6-1.2-1.2l0,0c0-0.7,0.6-1.2,1.2-1.2h22.5c0.7,0,1.2,0.6,1.2,1.2l0,0C62.5,35.7,61.9,36.2,61.2,36.2z M62.5,44L62.5,44c0-0.7-0.6-1.2-1.2-1.2H38.8c-0.7,0-1.2,0.6-1.2,1.2l0,0c0,0.7,0.6,1.2,1.2,1.2h22.5C61.9,45.3,62.5,44.7,62.5,44z M62.5,26L62.5,26c0-0.7-0.6-1.3-1.2-1.3H38.8c-0.7,0-1.2,0.6-1.2,1.2v0c0,0.7,0.6,1.2,1.2,1.2h22.5C61.9,27.2,62.5,26.7,62.5,26z"></path></svg>',
-		'variant-2' => '<svg width="100%" viewBox="0 0 100 70"><path d="M61.2,36.2H38.8c-0.7,0-1.2-0.6-1.2-1.2l0,0c0-0.7,0.6-1.2,1.2-1.2h22.5c0.7,0,1.2,0.6,1.2,1.2l0,0C62.5,35.7,61.9,36.2,61.2,36.2z M56.3,43L56.3,43c0-0.7-0.6-1.3-1.2-1.3H38.8c-0.7,0-1.2,0.6-1.2,1.2v0c0,0.7,0.6,1.2,1.2,1.2H55C55.7,44.3,56.3,43.7,56.3,43z M50,27L50,27c0-0.7-0.6-1.2-1.2-1.2h-10c-0.7,0-1.2,0.6-1.2,1.2v0c0,0.7,0.6,1.2,1.2,1.2h10C49.4,28.2,50,27.7,50,27z"></path></svg>',
-		'variant-3' => '<svg width="100%" viewBox="0 0 100 70"><path d="M61.2,36.2H38.8c-0.7,0-1.2-0.6-1.2-1.2l0,0c0-0.7,0.6-1.2,1.2-1.2h22.5c0.7,0,1.2,0.6,1.2,1.2l0,0C62.5,35.7,61.9,36.2,61.2,36.2z M57.9,43L57.9,43c0-0.7-0.6-1.3-1.2-1.3H40.8c-0.7,0-1.2,0.6-1.2,1.2v0c0,0.7,0.6,1.2,1.2,1.2h15.9C57.4,44.3,57.9,43.7,57.9,43z M60.5,27L60.5,27c0-0.7-0.6-1.2-1.2-1.2H44.9c-0.7,0-1.2,0.6-1.2,1.2v0c0,0.7,0.6,1.2,1.2,1.2h14.3C59.9,28.2,60.5,27.7,60.5,27z"></path></svg>',
+		'variant-1' => '
+		<svg class="ct-icon" width="18" height="14" viewBox="0 0 18 14" fill="currentColor" aria-hidden="true" data-variant="variant-1">
+			<rect y="0.00" width="18" height="1.7" rx="1"></rect>
+			<rect y="6.15" width="18" height="1.7" rx="1"></rect>
+			<rect y="12.3" width="18" height="1.7" rx="1"></rect>
+		</svg>
+		',
+		'variant-2' => '
+		<svg class="ct-icon" width="18" height="14" viewBox="0 0 18 14" fill="currentColor" aria-hidden="true" data-variant="variant-2">
+			<rect y="0.00" width="10" height="1.7" rx="1"></rect>
+			<rect y="6.15" width="18" height="1.7" rx="1"></rect>
+			<rect y="12.3" width="15" height="1.7" rx="1"></rect>
+		</svg>
+		',
+		'variant-3' => '
+		<svg class="ct-icon" width="18" height="14" viewBox="0 0 18 14" fill="currentColor" aria-hidden="true" data-variant="variant-3">
+			<rect y="0.00" x="6.00" width="12" height="1.7" rx="1"></rect>
+			<rect y="6.15" width="18" height="1.7" rx="1"></rect>
+			<rect y="12.3" width="12" height="1.7" rx="1"></rect>
+		</svg>
+		',
 	] )
 	->defaultValue( 'variant-1' )
 	->transport( 'postMessage' )
@@ -63,7 +81,25 @@ wpbf_customizer_field()
 	->id( $control_id_prefix . 'text' )
 	->type( 'text' )
 	->tab( 'general' )
-	->label( __( 'Text', 'page-builder-framework' ) )
-	->defaultValue( 'Menu' )
+	->label( __( 'Label', 'page-builder-framework' ) )
+	->transport( 'postMessage' )
+	->properties( array(
+		'input_attrs' => array(
+			'placeholder' => __( 'Menu', 'page-builder-framework' ),
+		),
+	) )
+	->addToSection( $section_id );
+
+wpbf_customizer_field()
+	->id( $control_id_prefix . 'style' )
+	->type( 'radio-buttonset' )
+	->tab( 'general' )
+	->label( __( 'Style', 'page-builder-framework' ) )
+	->defaultValue( '' )
+	->choices( [
+		''        => 'Simple',
+		'outline' => 'Outlined',
+		'solid'   => 'Solid',
+	] )
 	->transport( 'postMessage' )
 	->addToSection( $section_id );
