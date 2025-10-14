31e94a4a2be33c4f4d5c1766903510e87f3bd635
Dwi <dwie.cendhol@gmail.com>
2025-10-14 14:14:27 +0700

    add mobile menu padding

--- inc/customizer/js/customizer-parts/setup-controls-movement.ts
+++ inc/customizer/js/customizer-parts/setup-controls-movement.ts
@@ -218,59 +218,11 @@ export function setupControlsMovement() {
 							to: 14,
 						},
 					},
-				],
-			},
-						{
-				from: "wpbf_mobile_menu_options",
-				to: "wpbf_header_builder_mobile_menu_2_section",
-				controlsToMove: [
 					{
-						id: "mobile_menu_bg_color",
+						id: "mobile_menu_padding",
 						prio: {
 							from: undefined,
-							to: 8,
-						},
-					},
-					{
-						id: "mobile_menu_bg_color_alt",
-						prio: {
-							from: undefined,
-							to: 9,
-						},
-					},
-					{
-						id: "mobile_menu_font_color",
-						prio: {
-							from: undefined,
-							to: 10,
-						},
-					},
-					{
-						id: "mobile_menu_font_color_alt",
-						prio: {
-							from: undefined,
-							to: 11,
-						},
-					},
-					{
-						id: "mobile_menu_border_color",
-						prio: {
-							from: undefined,
-							to: 12,
-						},
-					},
-					{
-						id: "mobile_menu_submenu_arrow_color",
-						prio: {
-							from: undefined,
-							to: 13,
-						},
-					},
-					{
-						id: "mobile_menu_font_size",
-						prio: {
-							from: undefined,
-							to: 14,
+							to: 15,
 						},
 					},
 				],
