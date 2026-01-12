1e17f5072e364afdffd32c59bd3a99dda14df97d
Bagus <25834188+contactjavas@users.noreply.github.com>
2026-01-13 00:55:18 +0700

    Fix HTML widget live preview in footer
    Changed HTML widget settings from partialRefresh to postMessage transport for all 4 section files (desktop/mobile html-1/html-2). This prevents the footer from resetting when editing HTML widget content.

--- inc/customizer/settings/footer-builder/mobile/html-1-section.php
+++ inc/customizer/settings/footer-builder/mobile/html-1-section.php
@@ -19,23 +19,11 @@ wpbf_customizer_section()
 
 $control_id_prefix = 'wpbf_footer_builder_mobile_html_1_';
 
-$partial_refresh_key_prefix = 'footerbuilder_mobile_html_1_';
-
-$partial_refresh_args = array(
-	'container_inclusive' => true,
-	'selector'            => '#footer',
-	'render_callback'     => function () {
-		return get_template_part( 'inc/template-parts/footer' );
-	},
-);
-
 wpbf_customizer_field()
 	->id( $control_id_prefix . 'content' )
 	->type( 'editor' )
 	->defaultValue( __( 'Content for widget HTML 1', 'page-builder-framework' ) )
-	->partialRefresh( [
-		$partial_refresh_key_prefix . 'content' => $partial_refresh_args,
-	] )
+	->transport( 'postMessage' )
 	->properties( array(
 		'tinymce' => array(
 			'toolbar1' => 'formatselect,styleselect,numlist,bullist,removeformat,bold,italic,underline,strikethrough,alignleft,aligncenter,alignright,link,unlink,forecolor,backcolor',
